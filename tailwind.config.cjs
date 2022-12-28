719feedf18e7ffa67a9014304a997e32ab0432e1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-28 10:31:59 +0330

    Defined root colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#EA4C22",
+        white: "#FFFEFA",
+        black: "#101010",
+      },
+    },
   },
   plugins: [],
-}
+};
