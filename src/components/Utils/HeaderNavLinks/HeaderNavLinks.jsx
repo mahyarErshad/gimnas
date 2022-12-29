9d292a6e802f9fe91cb9f6d946eae50bafe4ba06
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-29 08:33:20 +0330

    Component is dynamic

--- src/components/Utils/HeaderNavLinks/HeaderNavLinks.jsx
+++ src/components/Utils/HeaderNavLinks/HeaderNavLinks.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-function HeaderNavLinks() {
+function HeaderNavLinks(props) {
+  const color = props.color || "text-white";
   return (
     <li className="relative">
-      <a href="#" className=" hover:text-[#bbb] text-white text-xl px-3 py-2 font-semibold nav-items">
-        مستندات
+      <a href="#" className={`hover:text-[#bbb] ${color} text-xl px-3 py-2 font-semibold nav-items`}>
+        {props.title}
       </a>
     </li>
   );
