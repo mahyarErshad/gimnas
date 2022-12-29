08bf160d7892d91a46e53c76e2931cf4b420697a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-29 09:25:48 +0330

    Wrapper is styled

--- src/components/Header/HeaderTextSection/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection/HeaderTextSection.jsx
@@ -1,7 +1,14 @@
 import React, { memo } from "react";
 
 function HeaderTextSection() {
-  return <div className="text-white w-full h-full flex justify-center items-center">HeaderTextSection</div>;
+  return (
+    <div className="text-white w-full h-full flex justify-center items-center">
+      <div className="flex justify-center items-center flex-col gap-2 bg-primary rounded-[50%] w-[12.5rem] h-[12.5rem]">
+        <span>12</span>
+        <span>Home Pages</span>
+      </div>
+    </div>
+  );
 }
 
 export default memo(HeaderTextSection);
