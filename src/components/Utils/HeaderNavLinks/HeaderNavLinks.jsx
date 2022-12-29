1f24d1a8bfcbedfb166d1c54594ed66ce69f6efc
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-29 09:02:56 +0330

    Increased pt on max lg

--- src/components/Utils/HeaderNavLinks/HeaderNavLinks.jsx
+++ src/components/Utils/HeaderNavLinks/HeaderNavLinks.jsx
@@ -3,7 +3,7 @@ import React, { memo } from "react";
 function HeaderNavLinks(props) {
   const color = props.color || "text-white";
   return (
-    <li className="relative">
+    <li className="relative max-lg:pt-4">
       <a href="#" className={`hover:text-[#bbb] ${color} text-lg px-3 py-2 font-semibold nav-items whitespace-nowrap`}>
         {props.title}
       </a>
