69d4bc9c6ee518d5f16aecd97f644a79ceeec6f8
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-29 10:06:26 +0330

    Added a p tag to the end

--- src/components/Header/HeaderTextSection/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection/HeaderTextSection.jsx
@@ -2,13 +2,15 @@ import React, { memo } from "react";
 
 function HeaderTextSection() {
   return (
-    <div className="text-white w-full h-full flex justify-start flex-col items-center max-lg:pt-16 lg:pt-36">
+    <div className="text-white w-full h-full flex justify-start flex-col items-center max-lg:gap-6 lg:gap-8 max-lg:pt-16 lg:pt-36">
       <div className="flex justify-center items-center flex-col bg-primary rounded-[50%] w-[12.5rem] h-[12.5rem] relative">
         <span className="text-[5rem] font-black absolute top-[12%]">12</span>
         <span className="text-lg font-black absolute top-[60%]">صفحه اصلی</span>
       </div>
       <div className="flex justify-center items-center">
-      <p>جیمناس - تم وردپرسی ورزشی</p>
+        <p className="font-bold max-md:text-xl md:text-3xl lg:text-5xl text-center">
+          جیمناس - <span className="text-inherit text-primary font-bold">محبوب ترین قالب باشگاهی ورزشی </span> وردپرس
+        </p>
       </div>
     </div>
   );
