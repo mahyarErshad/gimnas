e34a9d6eed432869c80164561851e670d6a03909
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-29 10:20:29 +0330

    Catching href from props

--- src/components/Utils/Buttons/CustomButton/CustomButton.jsx
+++ src/components/Utils/Buttons/CustomButton/CustomButton.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-function CustomButton() {
+function CustomButton(props) {
+    const href = props.href || "#"
   return (
-    <a href="#">
+    <a href={href}>
       <button className="bg-primary transition-all hover:opacity-90 max-md:py-3 md:py-4 max-md:px-6 md:px-8 max-md:text-base md:text-lg lg:text-xl font-medium rounded-[1.875rem] text-center">مشاهده همه دموها</button>
     </a>
   );
