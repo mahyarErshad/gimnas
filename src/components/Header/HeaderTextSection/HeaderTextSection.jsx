92a8777a252ccea6117a0fb3ee7bcc39d41e8545
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-29 10:25:21 +0330

    12 is persian

--- src/components/Header/HeaderTextSection/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection/HeaderTextSection.jsx
@@ -5,7 +5,7 @@ function HeaderTextSection() {
   return (
     <div className="text-white w-full h-full flex justify-start flex-col items-center max-lg:gap-6 lg:gap-8 max-lg:pt-16 lg:pt-36">
       <div className="flex justify-center items-center flex-col bg-primary rounded-[50%] w-[12.5rem] h-[12.5rem] relative">
-        <span className="text-[5rem] font-black absolute top-[12%]">12</span>
+        <span className="text-[5rem] font-black absolute top-[12%]">۱۲</span>
         <span className="text-lg font-black absolute top-[60%]">صفحه اصلی</span>
       </div>
       <div className="flex justify-center items-center px-4">
