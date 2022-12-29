9199b8c2d678024871740be0281dbcf6cb0a82f1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-29 09:42:57 +0330

    !2 home pages is done

--- src/components/Header/HeaderTextSection/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection/HeaderTextSection.jsx
@@ -2,10 +2,10 @@ import React, { memo } from "react";
 
 function HeaderTextSection() {
   return (
-    <div className="text-white w-full h-full flex justify-center items-center">
-      <div className="flex justify-center items-center flex-col gap-2 bg-primary rounded-[50%] w-[12.5rem] h-[12.5rem]">
-        <span>12</span>
-        <span>Home Pages</span>
+    <div className="text-white w-full h-full flex justify-center items-start max-lg:pt-16 lg:pt-36">
+      <div className="flex justify-center items-center flex-col bg-primary rounded-[50%] w-[12.5rem] h-[12.5rem] relative">
+        <span className="text-[5rem] font-black absolute top-[12%]">12</span>
+        <span className="text-lg font-black absolute top-[60%]">صفحه اصلی</span>
       </div>
     </div>
   );
