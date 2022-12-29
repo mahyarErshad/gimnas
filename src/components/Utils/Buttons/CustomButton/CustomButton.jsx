73ac2fef60c12ce97fb0522aea79830737f6aa67
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-29 10:19:37 +0330

    Copied codes

--- src/components/Utils/Buttons/CustomButton/CustomButton.jsx
+++ src/components/Utils/Buttons/CustomButton/CustomButton.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function CustomButton() {
   return (
-    <div>CustomButton</div>
-  )
+    <a href="#">
+      <button className="bg-primary transition-all hover:opacity-90 max-md:py-3 md:py-4 max-md:px-6 md:px-8 max-md:text-base md:text-lg lg:text-xl font-medium rounded-[1.875rem] text-center">مشاهده همه دموها</button>
+    </a>
+  );
 }
 
-export default CustomButton
+export default CustomButton;
