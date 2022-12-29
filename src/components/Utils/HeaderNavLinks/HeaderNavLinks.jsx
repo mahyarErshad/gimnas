cbcdde3bea09e4fa31fde47fa078d05cea51cc5d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-29 08:31:37 +0330

    Copied the codes

--- src/components/Utils/HeaderNavLinks/HeaderNavLinks.jsx
+++ src/components/Utils/HeaderNavLinks/HeaderNavLinks.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function HeaderNavLinks() {
   return (
-    <div>HeaderNavLinks</div>
-  )
+    <li className="relative">
+      <a href="#" className=" hover:text-[#bbb] text-white text-xl px-3 py-2 font-semibold nav-items">
+        مستندات
+      </a>
+    </li>
+  );
 }
 
-export default HeaderNavLinks
+export default HeaderNavLinks;
