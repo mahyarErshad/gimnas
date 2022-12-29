33b0ff3dc352b9f17dc77d3fe0efdf41d6dca792
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-29 09:07:22 +0330

    bg color of the button is changed

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -28,7 +28,7 @@ function Navbar() {
             </div>
           </div>
           <div className="-mr-2 flex lg:hidden">
-            <button onClick={() => setIsOpen(!isOpen)} type="button" className="bg-gray-900 absolute right-[10%] top-[3.2%] inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+            <button onClick={() => setIsOpen(!isOpen)} type="button" className="bg-primary absolute right-[10%] top-[3.2%] inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
               {!isOpen ? (
                 <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
