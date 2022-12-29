d28aff7badebfe8023bec834138209728c86219f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-29 09:18:08 +0330

    Import HeaderTextSection

--- src/components/Header/MainHeader.jsx
+++ src/components/Header/MainHeader.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import HeaderTextSection from "./HeaderTextSection/HeaderTextSection";
 import Navbar from "./Navbar/Navbar";
 
 function MainHeader() {
   return (
     <header>
       <Navbar />
+      <HeaderTextSection />
     </header>
   );
 }
