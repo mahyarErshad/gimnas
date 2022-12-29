80035994dae153f5bc6b408026840d66a1e0cc5e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-29 10:23:50 +0330

    Catch href from props

--- src/components/Utils/HeaderNavLinks/HeaderNavLinks.jsx
+++ src/components/Utils/HeaderNavLinks/HeaderNavLinks.jsx
@@ -1,10 +1,11 @@
 import React, { memo } from "react";
 
 function HeaderNavLinks(props) {
+  const href = props.href || "#";
   const color = props.color || "text-white";
   return (
     <li className="relative max-lg:pt-4">
-      <a href="#" className={`hover:text-[#bbb] ${color} text-lg px-3 py-2 font-semibold nav-items whitespace-nowrap max-lg:mr-[13%] max-sm:mr-12`}>
+      <a href={href} className={`hover:text-[#bbb] ${color} text-lg px-3 py-2 font-semibold nav-items whitespace-nowrap max-lg:mr-[13%] max-sm:mr-12`}>
         {props.title}
       </a>
     </li>
