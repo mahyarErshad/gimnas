08b2774c31c424c9c61bead1bd867c66d63e285d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-29 08:53:09 +0330

    Giving whitespace no wrap

--- src/components/Utils/HeaderNavLinks/HeaderNavLinks.jsx
+++ src/components/Utils/HeaderNavLinks/HeaderNavLinks.jsx
@@ -4,7 +4,7 @@ function HeaderNavLinks(props) {
   const color = props.color || "text-white";
   return (
     <li className="relative">
-      <a href="#" className={`hover:text-[#bbb] ${color} text-lg px-3 py-2 font-semibold nav-items`}>
+      <a href="#" className={`hover:text-[#bbb] ${color} text-lg px-3 py-2 font-semibold nav-items whitespace-nowrap`}>
         {props.title}
       </a>
     </li>
