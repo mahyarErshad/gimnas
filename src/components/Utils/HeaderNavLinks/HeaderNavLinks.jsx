471d8d266d331917fc1129de0e217854bc298918
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-29 08:47:51 +0330

    Removed clg

--- src/components/Utils/HeaderNavLinks/HeaderNavLinks.jsx
+++ src/components/Utils/HeaderNavLinks/HeaderNavLinks.jsx
@@ -2,7 +2,6 @@ import React, { memo } from "react";
 
 function HeaderNavLinks(props) {
   const color = props.color || "text-white";
-  console.log(props.color);
   return (
     <li className="relative">
       <a href="#" className={`hover:text-[#bbb] ${color} text-lg px-3 py-2 font-semibold nav-items`}>
