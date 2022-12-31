e6014dbdd758e422c4b5a7f7fedf476bec1fde2f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-31 09:50:33 +0330

    second button's Icon is set

--- src/components/Header/HeaderTextSection/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection/HeaderTextSection.jsx
@@ -7,7 +7,7 @@ function HeaderTextSection() {
     <div className="text-white w-full h-full flex justify-start flex-col items-center max-lg:gap-6 lg:gap-8 max-lg:pt-16 lg:pt-36">
       <HeaderTexts />
       <HeaderSliderButton title="پشتیبانی" image="https://img.icons8.com/windows/32/FFFFFF/help.png" top="top-[75%]" />
-      <HeaderSliderButton title="خرید قالب" image="https://img.icons8.com/windows/32/FFFFFF/help.png" top="top-[81.5%]" />
+      <HeaderSliderButton title="خرید قالب" image="https://img.icons8.com/pastel-glyph/32/FFFFFF/shopping-cart--v1.png" top="top-[81.5%]" />
     </div>
   );
 }
