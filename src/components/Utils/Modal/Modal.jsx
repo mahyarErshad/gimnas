a5e418e006077e7b09f2f6456f007c9b872e456d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-31 15:01:33 +0330

    Imported CloseIcon as component

--- src/components/Utils/Modal/Modal.jsx
+++ src/components/Utils/Modal/Modal.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
 import ModalContext from "../../../lib/context/ModalContext";
 import "./modal.css";
+import { ReactComponent as CloseIcon } from "../../../assets/images/closeIcon.svg";
 
 function Modal() {
   const modalContext = useContext(ModalContext);
   return (
     <div onClick={() => modalContext.setIsModalOpen(false)} className="fixed top-0 right-0 w-full h-full z-20 bg-[#232323] modal-animation opacity-[0.85]">
       <button>
-        <img src="" alt="close modal" />
+        <CloseIcon />
       </button>
     </div>
   );
