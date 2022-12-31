025f0617565e6f9e5d70436070498b1731256bef
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-31 14:20:09 +0330

    Created stylesheet for modal

--- src/components/Utils/Modal/Modal.jsx
+++ src/components/Utils/Modal/Modal.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Modal() {
-  return <div className="bg-red fixed top-0 right-0 w-full h-full z-20 bg-red-600">Modal</div>;
+  return <div className="bg-red fixed top-0 right-0 w-full h-full z-20 bg-[#232323] opacity-80"></div>;
 }
 
 export default Modal;
