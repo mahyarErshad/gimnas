b73b604bf2b6b3ca24c6de56f33980c24952f9b3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-31 09:25:32 +0330

    Using state

--- src/components/Utils/Buttons/HeaderSliderButton/HeaderSliderButton.jsx
+++ src/components/Utils/Buttons/HeaderSliderButton/HeaderSliderButton.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 function HeaderSliderButton(props) {
+    const [open, setOpen] = useState(false)
   return (
     <button className={`flex flex-nowrap items-center z-10 text-xl outline-none border-none w-[10.5rem] h-[3.125rem] absolute right-[-7.375rem] hover:opacity-90 transition-all duration-500 ${props.top}`}>
       <span className="bg-[#232323] text-white flex-grow h-full flex-full-center">{props.title}</span>
