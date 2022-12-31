5d4fba879e8069d9418e5219ad1ef14ca62bdc13
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-31 14:59:10 +0330

    Created burgerMenuIcon

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -32,9 +32,7 @@ function Navbar() {
             <button onClick={() => setIsOpen(!isOpen)} type="button" className="bg-primary absolute right-[10%] top-[3.2%] inline-flex items-center justify-center p-2 rounded-md text-white hover:opacity-90 transition-all" aria-controls="mobile-menu" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
               {!isOpen ? (
-                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-                </svg>
+
               ) : (
                 <CloseIcon />
               )}
