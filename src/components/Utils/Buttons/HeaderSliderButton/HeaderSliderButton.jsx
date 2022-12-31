c82c71b5943a9fec2c007a0869fb977c01329d49
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-31 09:21:11 +0330

    Getting opacity on hover

--- src/components/Utils/Buttons/HeaderSliderButton/HeaderSliderButton.jsx
+++ src/components/Utils/Buttons/HeaderSliderButton/HeaderSliderButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function HeaderSliderButton(props) {
   return (
-    <button className={`flex flex-nowrap items-center z-10 text-xl outline-none border-none w-[10.5rem] h-[3.125rem] absolute right-[-7.375rem] ${props.top}`}>
+    <button className={`flex flex-nowrap items-center z-10 text-xl outline-none border-none w-[10.5rem] h-[3.125rem] absolute right-[-7.375rem] hover:opacity-90 ${props.top}`}>
       <span className="bg-[#232323] text-white flex-grow h-full flex-full-center">{props.title}</span>
       <div className="h-full bg-primary w-[3.125rem] flex-full-center">
         <img className="flip" src={props.image} alt={props.title} />
