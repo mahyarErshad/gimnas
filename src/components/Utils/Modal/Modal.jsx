afd4c37dcd526416fb4e7662be2564c784806cb8
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-31 15:11:20 +0330

    Added modal's close button

--- src/components/Utils/Modal/Modal.jsx
+++ src/components/Utils/Modal/Modal.jsx
@@ -8,7 +8,7 @@ function Modal() {
   return (
     <div onClick={() => modalContext.setIsModalOpen(false)} className="fixed top-0 right-0 w-full h-full z-20 bg-[#232323] modal-animation opacity-[0.85]">
       <button>
-        <CloseIcon />
+        <CloseIcon color="#fff" className="w-10 h-10 p-3 absolute right-12 top-12 bg-[#ffffff1a] rounded-[50%]" />
       </button>
     </div>
   );
