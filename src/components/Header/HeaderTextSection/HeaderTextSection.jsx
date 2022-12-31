0822837bff090d281be1620286011e247d23e1f5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-31 10:56:18 +0330

    Catch modalIsOpen from context

--- src/components/Header/HeaderTextSection/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection/HeaderTextSection.jsx
@@ -1,8 +1,10 @@
-import React, { memo } from "react";
+import React, { memo, useContext } from "react";
+import CustomContext from "../../../lib/context/CustomContext";
 import HeaderSliderButton from "../../Utils/Buttons/HeaderSliderButton/HeaderSliderButton";
 import HeaderTexts from "./HeaderTexts";
 
 function HeaderTextSection() {
+  const {modalIsOpen} = useContext(CustomContext);
   return (
     <div className="text-white w-full h-full flex justify-start flex-col items-center max-lg:gap-6 lg:gap-8 max-lg:pt-16 lg:pt-36">
       <HeaderTexts />
