9244e139661bcdeef0865a941fc2622bd2912815
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-31 08:31:04 +0330

    Imported HeaderTexts

--- src/components/Header/HeaderTextSection/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection/HeaderTextSection.jsx
@@ -1,9 +1,10 @@
 import React, { memo } from "react";
+import HeaderTexts from "./HeaderTexts";
 
 function HeaderTextSection() {
   return (
     <div className="text-white w-full h-full flex justify-start flex-col items-center max-lg:gap-6 lg:gap-8 max-lg:pt-16 lg:pt-36">
-
+      <HeaderTexts />
     </div>
   );
 }
