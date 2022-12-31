360992cf16f33392f28cef265eab88047b338324
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-31 09:09:11 +0330

    Created flex-full-center class

--- src/components/Utils/Buttons/HeaderSliderButton/HeaderSliderButton.jsx
+++ src/components/Utils/Buttons/HeaderSliderButton/HeaderSliderButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function HeaderSliderButton(props) {
   return (
     <button className="flex flex-nowrap items-center z-10 text-xl outline-none border-none w-[10.5rem] h-[3.125rem]">
-      <span className="bg-[#232323] text-white flex-grow h-full">{props.title}</span>
+      <span className="bg-[#232323] text-white flex-grow h-full flex items-center justify-center">{props.title}</span>
       <div className="h-full bg-primary w-[3.125rem]">
         <img className="flip" src={props.image} alt={props.title} />
       </div>
