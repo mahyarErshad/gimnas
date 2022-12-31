aa9877b61d72376e143899a8cbe85a26ca008827
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-31 09:51:50 +0330

    Get onClick function from props

--- src/components/Utils/Buttons/HeaderSliderButton/HeaderSliderButton.jsx
+++ src/components/Utils/Buttons/HeaderSliderButton/HeaderSliderButton.jsx
@@ -5,7 +5,7 @@ function HeaderSliderButton(props) {
   const right = open ? "right-0" : "right-[-7.375rem]";
   return (
     <button onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)} className={`flex flex-nowrap items-center z-10 text-xl outline-none border-none w-[10.5rem] h-[3.125rem] absolute ${right} hover:opacity-90 transition-all duration-500 ${props.top}`}>
-      <span className="bg-[#232323] text-white flex-grow h-full flex-full-center">{props.title}</span>
+      <span onClick={props.onClick} className="bg-[#232323] text-white flex-grow h-full flex-full-center">{props.title}</span>
       <div className="h-full bg-primary w-[3.125rem] flex-full-center">
         <img className="flip" src={props.image} alt={props.title} />
       </div>
