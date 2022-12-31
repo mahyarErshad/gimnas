f55c79442b6b77ccf4a7e949a13909c69c7ac7b3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-31 09:05:01 +0330

    Working with classes

--- src/components/Utils/Buttons/HeaderSliderButton/HeaderSliderButton.jsx
+++ src/components/Utils/Buttons/HeaderSliderButton/HeaderSliderButton.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function HeaderSliderButton(props) {
   return (
-    <button className="flex flex-row-reverse flex-nowrap items-center z-10">
-      <img className="max-w-[3.125rem] flip" src={props.image} alt={props.title} />
-      <span>{props.title}</span>
+    <button className="flex flex-nowrap items-center z-10 text-xl outline-none border-none w-[10.5rem] h-[3.125rem]">
+      <span className="bg-[#232323] text-white flex-grow h-full">{props.title}</span>
+      <img className="w-[3.125rem] h-full flip bg-primary" src={props.image} alt={props.title} />
     </button>
   );
 }
