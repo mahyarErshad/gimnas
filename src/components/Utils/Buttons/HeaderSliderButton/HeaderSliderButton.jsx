a4596e5d17bd63a4c63b34f633d0969377ad59e8
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-31 09:29:52 +0330

    Defined changeButtonVisibilityHandler function

--- src/components/Utils/Buttons/HeaderSliderButton/HeaderSliderButton.jsx
+++ src/components/Utils/Buttons/HeaderSliderButton/HeaderSliderButton.jsx
@@ -3,8 +3,11 @@ import React, { memo, useState } from "react";
 function HeaderSliderButton(props) {
   const [open, setOpen] = useState(false);
   const right = open ? "right-0" : "right-[-7.375rem]";
+  function changeButtonVisibilityHandler() {
+    setOpen((prev) => !prev);
+  }
   return (
-    <button className={`flex flex-nowrap items-center z-10 text-xl outline-none border-none w-[10.5rem] h-[3.125rem] absolute ${right} hover:opacity-90 transition-all duration-500 ${props.top}`}>
+    <button onMouseEnter={() => changeButtonVisibilityHandler} className={`flex flex-nowrap items-center z-10 text-xl outline-none border-none w-[10.5rem] h-[3.125rem] absolute ${right} hover:opacity-90 transition-all duration-500 ${props.top}`}>
       <span className="bg-[#232323] text-white flex-grow h-full flex-full-center">{props.title}</span>
       <div className="h-full bg-primary w-[3.125rem] flex-full-center">
         <img className="flip" src={props.image} alt={props.title} />
