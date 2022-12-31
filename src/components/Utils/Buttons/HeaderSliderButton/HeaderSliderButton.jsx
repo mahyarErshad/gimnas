66e23814d62a558b5d34747e1074e00f69b7248d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-31 09:08:07 +0330

    Changed the icon

--- src/components/Utils/Buttons/HeaderSliderButton/HeaderSliderButton.jsx
+++ src/components/Utils/Buttons/HeaderSliderButton/HeaderSliderButton.jsx
@@ -4,7 +4,9 @@ function HeaderSliderButton(props) {
   return (
     <button className="flex flex-nowrap items-center z-10 text-xl outline-none border-none w-[10.5rem] h-[3.125rem]">
       <span className="bg-[#232323] text-white flex-grow h-full">{props.title}</span>
-      <img className="w-[3.125rem] h-full flip bg-primary" src={props.image} alt={props.title} />
+      <div className="h-full bg-primary w-[3.125rem]">
+        <img className="flip" src={props.image} alt={props.title} />
+      </div>
     </button>
   );
 }
