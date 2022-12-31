ffa904eed1bd5c452c457a663df20598ed44891a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-31 08:46:15 +0330

    Import HeaderSliderButton

--- src/components/Header/HeaderTextSection/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection/HeaderTextSection.jsx
@@ -1,10 +1,12 @@
 import React, { memo } from "react";
+import HeaderSliderButton from "../../Utils/Buttons/HeaderSliderButton/HeaderSliderButton";
 import HeaderTexts from "./HeaderTexts";
 
 function HeaderTextSection() {
   return (
     <div className="text-white w-full h-full flex justify-start flex-col items-center max-lg:gap-6 lg:gap-8 max-lg:pt-16 lg:pt-36">
       <HeaderTexts />
+      <HeaderSliderButton />
     </div>
   );
 }
