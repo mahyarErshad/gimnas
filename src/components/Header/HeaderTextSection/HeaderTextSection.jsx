f400c097041b243cb4bd8d323ae56aec1dc28a70
Mahyar Ershad <mahyar.ershad92@gmail.com>
2022-12-31 09:46:53 +0330

    Buttons position is set

--- src/components/Header/HeaderTextSection/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection/HeaderTextSection.jsx
@@ -6,7 +6,8 @@ function HeaderTextSection() {
   return (
     <div className="text-white w-full h-full flex justify-start flex-col items-center max-lg:gap-6 lg:gap-8 max-lg:pt-16 lg:pt-36">
       <HeaderTexts />
-      <HeaderSliderButton title="پشتیبانی" image="https://img.icons8.com/windows/32/FFFFFF/help.png" top="top-[80%]" />
+      <HeaderSliderButton title="پشتیبانی" image="https://img.icons8.com/windows/32/FFFFFF/help.png" top="top-[75%]" />
+      <HeaderSliderButton title="خرید قالب" image="https://img.icons8.com/windows/32/FFFFFF/help.png" top="top-[81.5%]" />
     </div>
   );
 }
