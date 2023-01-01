c934a68fb743f124f7265239b341a4c40e2a20c1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-01 11:46:04 +0330

    Added the arrow image

--- src/components/Utils/Buttons/InternalNavigationButton/InternalNavigationButton.jsx
+++ src/components/Utils/Buttons/InternalNavigationButton/InternalNavigationButton.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function InternalNavigationButton() {
-  return <div className="w-7 h-14 border-[1px] rounded-[20px] absolute bottom-[5%]"></div>;
+function InternalNavigationButton(props) {
+  return (
+    <a href={props.href || "#"} className="w-7 flex-full-center h-14 border-[1px] rounded-[20px] absolute bottom-[5%]">
+      <img src="https://img.icons8.com/ios-filled/18/FFFFFF/double-down.png" alt="arrow" />
+    </a>
+  );
 }
 
 export default InternalNavigationButton;
