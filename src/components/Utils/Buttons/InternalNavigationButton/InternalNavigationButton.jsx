71d68a68a1a14293de44dfdcbe47dc3485f2fa9c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-01 11:40:07 +0330

    Wrapper is positioned

--- src/components/Utils/Buttons/InternalNavigationButton/InternalNavigationButton.jsx
+++ src/components/Utils/Buttons/InternalNavigationButton/InternalNavigationButton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function InternalNavigationButton() {
-  return <div className="w-7 h-14 border-[1px solid white] rounded-[20px]"></div>;
+  return <div className="w-7 h-14 border-[1px] rounded-[20px] absolute bottom-[5%]"></div>;
 }
 
 export default InternalNavigationButton;
