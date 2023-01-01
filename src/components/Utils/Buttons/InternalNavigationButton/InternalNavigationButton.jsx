e9796b1c52790dbf9d94b79f1369fe5dd292f55f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-01 11:50:39 +0330

    Added the animation

--- src/components/Utils/Buttons/InternalNavigationButton/InternalNavigationButton.jsx
+++ src/components/Utils/Buttons/InternalNavigationButton/InternalNavigationButton.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import styles from "./InternalNavigationButton.module.css";
+import "./InternalNavigationButton.css";
 
 function InternalNavigationButton(props) {
   return (
     <a href={props.href || "#"} className="w-7 flex-full-center h-14 border-[1px] rounded-[20px] absolute bottom-[5%]">
-      <img src="https://img.icons8.com/ios-filled/18/FFFFFF/double-down.png" alt="arrow" />
+      <img className="arrow-animation" src="https://img.icons8.com/ios-filled/18/FFFFFF/double-down.png" alt="arrow" />
     </a>
   );
 }
