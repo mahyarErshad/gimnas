7178f7d0594b976fb029d6526b56c5533bfacc02
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-01 09:53:19 +0330

    Modal animation has been editted

--- src/components/Utils/Modal/Modal.jsx
+++ src/components/Utils/Modal/Modal.jsx
@@ -5,11 +5,15 @@ import { ReactComponent as CloseIcon } from "../../../assets/images/closeIcon.sv
 
 function Modal() {
   const modalContext = useContext(ModalContext);
-  const [closing, setClosing] = useState(false)
+  const [closing, setClosing] = useState(false);
+  const animation = closing ? "modal-animation__close" : "modal-animation__open";
+  const handleCloseModal = () => {
+    setClosing(true);
+  };
   return (
-    <div className="fixed top-0 right-0 w-full h-full z-20 bg-[#232323] modal-animation opacity-[0.85]">
+    <div className={`fixed top-0 right-0 w-full h-full z-20 bg-[#232323] opacity-[0.85] modal-animation__open ${animation}`}>
       <button>
-        <CloseIcon onClick={() => modalContext.setIsModalOpen(false)} width="3.125rem" color="#fff" className="p-3 absolute right-12 top-12 bg-[#ffffff1a] rounded-[50%] hover:bg-[#ffffff33] hover:text-[#eee] transition-all" />
+        <CloseIcon onClick={handleCloseModal} width="3.125rem" color="#fff" className="p-3 absolute right-12 top-12 bg-[#ffffff1a] rounded-[50%] hover:bg-[#ffffff33] hover:text-[#eee] transition-all" />
       </button>
     </div>
   );
