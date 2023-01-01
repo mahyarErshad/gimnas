11197c661327256e4795045e84a2b73a5efe9e47
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-01 11:33:49 +0330

    Imported InternalNavigationButton

--- src/components/Header/HeaderTextSection/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection/HeaderTextSection.jsx
@@ -1,6 +1,7 @@
 import React, { memo, useContext } from "react";
 import ModalContext from "../../../lib/context/ModalContext";
 import HeaderSliderButton from "../../Utils/Buttons/HeaderSliderButton/HeaderSliderButton";
+import InternalNavigationButton from "../../Utils/Buttons/InternalNavigationButton/InternalNavigationButton";
 import HeaderTexts from "./HeaderTexts";
 
 function HeaderTextSection() {
@@ -10,6 +11,7 @@ function HeaderTextSection() {
       <HeaderTexts />
       <HeaderSliderButton onClick={() => {modalContext.setIsModalOpen(true)}} title="پشتیبانی" image="https://img.icons8.com/windows/32/FFFFFF/help.png" top="top-[75%]" />
       <HeaderSliderButton onClick={() => window.open("https://www.rtl-theme.com/gimnas-sport-worpress-theme/", "_blank")} title="خرید قالب" image="https://img.icons8.com/external-creatype-outline-colourcreatype/28/FFFFFF/external-basket-miscellaneous-user-interface-v2-creatype-outline-colourcreatype.png" top="top-[81.5%]" />
+      <InternalNavigationButton />
     </div>
   );
 }
