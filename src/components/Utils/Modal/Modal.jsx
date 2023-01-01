b2b9d60a51ab2f47e8f3c324a51697cfa01618ae
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-01 09:35:49 +0330

    Added hover style to the button

--- src/components/Utils/Modal/Modal.jsx
+++ src/components/Utils/Modal/Modal.jsx
@@ -6,9 +6,9 @@ import { ReactComponent as CloseIcon } from "../../../assets/images/closeIcon.sv
 function Modal() {
   const modalContext = useContext(ModalContext);
   return (
-    <div onClick={() => modalContext.setIsModalOpen(false)} className="fixed top-0 right-0 w-full h-full z-20 bg-[#232323] modal-animation opacity-[0.85]">
+    <div className="fixed top-0 right-0 w-full h-full z-20 bg-[#232323] modal-animation opacity-[0.85]">
       <button>
-        <CloseIcon color="#fff" className="w-10 h-10 p-3 absolute right-12 top-12 bg-[#ffffff1a] rounded-[50%]" />
+        <CloseIcon onClick={() => modalContext.setIsModalOpen(false)} width="3.125rem" color="#fff" className="p-3 absolute right-12 top-12 bg-[#ffffff1a] rounded-[50%] hover:bg-[#ffffff33] hover:text-[#eee] transition-all" />
       </button>
     </div>
   );
