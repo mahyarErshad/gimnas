1330566f5c6b789293b084e6441a988ea4493800
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-01 10:23:17 +0330

    Modal is done

--- src/components/Utils/Modal/Modal.jsx
+++ src/components/Utils/Modal/Modal.jsx
@@ -6,9 +6,13 @@ import { ReactComponent as CloseIcon } from "../../../assets/images/closeIcon.sv
 function Modal() {
   const modalContext = useContext(ModalContext);
   return (
-    <div className="fixed top-0 right-0 w-full h-full z-20 bg-[#232323] opacity-[0.85] modal-animation__open">
+    <div className="fixed flex-full-center flex-col gap-10 top-0 right-0 w-full h-full z-20 bg-[#242423f5] modal-animation__open">
       <button>
         <CloseIcon onClick={() => modalContext.setIsModalOpen(false)} width="3.125rem" color="#fff" className="p-3 absolute right-12 top-12 bg-[#ffffff1a] rounded-[50%] hover:bg-[#ffffff33] hover:text-[#eee] transition-all" />
+      </button>
+      <h1 className="text-5xl text-white font-semibold opacity-100">نیاز به مشاوره دارید؟</h1>
+      <button className="text-white text-3xl font-normal hover:opacity-80 transition-all" onClick={() => window.open("https://www.rtl-theme.com/gimnas-sport-worpress-theme/discussions/", "_blank")}>
+        ارتباط با پشتیبانی
       </button>
     </div>
   );
