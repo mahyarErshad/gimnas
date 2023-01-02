fe9510380a85701fb0bf092bc86de5fd288aad88
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-02 11:04:29 +0330

    z-40

--- src/components/Utils/Buttons/CustomButton/CustomButton.jsx
+++ src/components/Utils/Buttons/CustomButton/CustomButton.jsx
@@ -4,7 +4,7 @@ function CustomButton(props) {
     const href = props.href || "#"
   return (
     <a href={href}>
-      <button className="bg-primary transition-all hover:opacity-90 max-md:py-3 md:py-4 max-md:px-6 md:px-8 max-md:text-base md:text-lg lg:text-xl font-medium rounded-[1.875rem] text-center">{props.title}</button>
+      <button className="bg-primary z-40 transition-all hover:opacity-90 max-md:py-3 md:py-4 max-md:px-6 md:px-8 max-md:text-base md:text-lg lg:text-xl font-medium rounded-[1.875rem] text-center">{props.title}</button>
     </a>
   );
 }
