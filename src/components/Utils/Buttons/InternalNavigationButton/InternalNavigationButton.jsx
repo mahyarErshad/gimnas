c7393d97bf16e957c33cd6eea1e2193ff23e11a5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-02 11:04:42 +0330

    z-40

--- src/components/Utils/Buttons/InternalNavigationButton/InternalNavigationButton.jsx
+++ src/components/Utils/Buttons/InternalNavigationButton/InternalNavigationButton.jsx
@@ -3,7 +3,7 @@ import "./InternalNavigationButton.css";
 
 function InternalNavigationButton(props) {
   return (
-    <a href={props.href || "#"} className="w-7 flex justify-center items-end h-14 border-[1px] rounded-[20px] absolute bottom-[5%] pb-1 transition-all hover:border-2 max-lg:hidden">
+    <a href={props.href || "#"} className="w-7 z-40 flex justify-center items-end h-14 border-[1px] rounded-[20px] absolute bottom-[5%] pb-1 transition-all hover:border-2 max-lg:hidden">
       <img className="arrow-animation" src="https://img.icons8.com/ios-filled/18/FFFFFF/double-down.png" alt="arrow" />
     </a>
   );
