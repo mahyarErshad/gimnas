053ba8dc14ed7eb0d8f0723ce8fbce14ab66eb8f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-02 11:28:46 +0330

    Fixed a bug

--- src/components/Header/HeaderTextSection/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection/HeaderTextSection.jsx
@@ -8,7 +8,7 @@ import HeaderTexts from "./HeaderTexts";
 function HeaderTextSection() {
   const modalContext = useContext(ModalContext);
   return (
-    <div id="HeaderTextSection" onMouseMove={animatedCircles} className="text-white w-full h-full flex justify-start flex-col items-center max-lg:gap-6 lg:gap-8 absolute max-lg:top-[35%] lg:top-[28%]">
+    <div id="HeaderTextSection" onMouseMove={animatedCircles} className="text-white w-full h-full overflow-hidden flex justify-start flex-col items-center max-lg:gap-6 lg:gap-8">
       <HeaderTexts />
       <HeaderSliderButton onClick={() => {modalContext.setIsModalOpen(true)}} title="پشتیبانی" image="https://img.icons8.com/windows/32/FFFFFF/help.png" top="top-[75%]" />
       <HeaderSliderButton onClick={() => window.open("https://www.rtl-theme.com/gimnas-sport-worpress-theme/", "_blank")} title="خرید قالب" image="https://img.icons8.com/external-creatype-outline-colourcreatype/28/FFFFFF/external-basket-miscellaneous-user-interface-v2-creatype-outline-colourcreatype.png" top="top-[81.5%]" />
