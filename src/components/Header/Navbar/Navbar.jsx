6e2da930f88e3a1817db45069ebeee3fb11e1398
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-02 10:47:30 +0330

    Changed the order

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar() {
   return (
     <nav className="pt-4">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-between flex-row-reverse h-16">
           <div className="flex items-center">
             <div className="flex-shrink-0"></div>
             <div className="hidden lg:block">
