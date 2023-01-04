7896245ebaa9761e1858b1d35c3bb7381cac87dd
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 15:38:12 +0330

    Copied the codes

--- src/components/Body/BodyPageLayout/BodyPagePhotos.jsx
+++ src/components/Body/BodyPageLayout/BodyPagePhotos.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function BodyPagePhotos() {
   return (
-    <div>BodyPagePhotos</div>
-  )
+    <a>
+      <img src={homeGymDefault} alt="homeGymDefault" className="max-md:w-[35rem] md:w-[21.5625rem] lg:w-[26.625rem]" />
+      <h5 className="text-center mt-4 font-medium text-xl">ص</h5>
+    </a>
+  );
 }
 
-export default BodyPagePhotos
+export default BodyPagePhotos;
