7562efe18ca08016ed27a8044be54795d7f20284
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 09:28:59 +0330

    Working on responsive

--- src/components/Body/BodyParts/PageCounter/PageCounter.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounter.jsx
@@ -3,7 +3,7 @@ import PageCounterComponent from "./PageCounterComponent";
 
 function PageCounter() {
   return (
-    <section className="bg-bgGray py-[5.625rem] flex justify-center items-center flex-wrap">
+    <section className="bg-bgGray py-[5.625rem] flex justify-center items-center flex-wrap max-md:px-4 md:px-8 lg:px-[10%] max-md:gap-6 max-lg:gap-16">
       <PageCounterComponent number="۱۲" text="تعداد صفحات اصلی" />
       <PageCounterComponent number="۰۶" text="صفحات اصلی متعدد" />
       <PageCounterComponent number="۰۶" text="صفحات اصلی تکی" />
--- src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function PageCounterComponent(props) {
   return (
-    <div className="lg:p-8 md:p-6 max-md:p-4 max-md:w-full md:w-2/4 lg:w-1/4 flex-col flex-full-center gap-8">
+    <div className="max-md:w-full md:w-[40%] lg:w-1/4 flex-col flex-full-center gap-8">
       <h3 className="text-[5rem] font-bold text-primary m-0 leading-4">{props.number}</h3>
       <h4 className="text-xl font-bold m-0">{props.text}</h4>
     </div>
