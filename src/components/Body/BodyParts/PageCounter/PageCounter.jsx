d103e477ff27ba39a05f4cc45e66b42e5bca4ad1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 10:00:24 +0330

    Uninstalled gsap

--- src/components/Body/BodyParts/PageCounter/PageCounter.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounter.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PageCounterComponent from "./PageCounterComponent";
 
 function PageCounter() {
-  
 
   return (
     <section className="bg-bgGray py-[5.625rem] flex justify-center items-center flex-wrap max-md:px-4 md:px-8 lg:px-[15%] max-lg:gap-20">
--- src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function PageCounterComponent(props) {
   return (
-    <div className="max-md:w-full md:w-[40%] lg:w-1/4 flex-col flex-full-center gap-8">
+    <div className="max-md:w-full md:w-[40%] lg:w-1/4 flex-col flex-full-center gap-8 PageCounterComponent">
       <h3 className="text-[5rem] font-bold text-primary m-0 leading-4 flex">{props.number} {props.plus && <span className="text-inherit font-bold">+</span>}</h3>
       <h4 className="text-xl font-bold m-0">{props.text}</h4>
     </div>
