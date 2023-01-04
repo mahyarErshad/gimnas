6213cf64ec72a1ffc5a16cac5233cbc235d1b638
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 15:39:48 +0330

    Using props.href

--- src/components/Body/BodyPageLayout/BodyPagePhotos.jsx
+++ src/components/Body/BodyPageLayout/BodyPagePhotos.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function BodyPagePhotos(props) {
   return (
-    <a>
+    <a href={props.href}>
       <img src={props.image} alt={props.title} className="max-md:w-[35rem] md:w-[21.5625rem] lg:w-[26.625rem]" />
       <h5 className="text-center mt-4 font-medium text-xl">{props.title}</h5>
     </a>
