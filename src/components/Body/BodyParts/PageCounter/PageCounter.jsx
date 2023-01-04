229c3ee792ba3689975f8c0b2b84b5a2e430e342
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 12:25:01 +0330

    installed react-intersection-observer

--- src/components/Body/BodyParts/PageCounter/PageCounter.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounter.jsx
@@ -3,12 +3,11 @@ import PageCounterComponent from "./PageCounterComponent";
 
 function PageCounter() {
   return (
-    <section className="bg-bgGray py-[5.625rem] flex justify-center items-center flex-wrap max-md:px-4 md:px-8 lg:px-[15%] max-lg:gap-20">
-
-        <PageCounterComponent number="۱۲" text="تعداد صفحات اصلی" />
-        <PageCounterComponent number="۰۶" text="صفحات اصلی چندگانه" />
-        <PageCounterComponent number="۰۶" text="صفحات اصلی تکی" />
-        <PageCounterComponent plus number="۴۶" text="صفحات داخلی" />
+    <section id="page-counter" className="bg-bgGray py-[5.625rem] flex justify-center items-center flex-wrap max-md:px-4 md:px-8 lg:px-[15%] max-lg:gap-20">
+      <PageCounterComponent number="۱۲" text="تعداد صفحات اصلی" />
+      <PageCounterComponent number="۰۶" text="صفحات اصلی چندگانه" />
+      <PageCounterComponent number="۰۶" text="صفحات اصلی تکی" />
+      <PageCounterComponent plus number="۴۶" text="صفحات داخلی" />
     </section>
   );
 }
