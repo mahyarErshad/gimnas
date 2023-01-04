a0b17cd569a30b3a6313c414f0d3cb8812928b85
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 09:10:13 +0330

    Imported PageCounterComponent

--- src/components/Body/BodyParts/PageCounter/PageCounter.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounter.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import PageCounterComponent from "./PageCounterComponent";
 
 function PageCounter() {
-  return <section className="bg-bgGray py-[5.625rem] flex justify-center items-center flex-wrap">PageCounter</section>;
+  return <section className="bg-bgGray py-[5.625rem] flex justify-center items-center flex-wrap">
+    <PageCounterComponent />
+  </section>;
 }
 
 export default PageCounter;
