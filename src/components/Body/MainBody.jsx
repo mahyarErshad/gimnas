c1a3ec43dd73a1610579bed2d1e4bca3f7a9b377
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 15:36:54 +0330

    Created template

--- src/components/Body/MainBody.jsx
+++ src/components/Body/MainBody.jsx
@@ -9,7 +9,7 @@ function MainBody() {
       <PageCounter />
       <BodyPageLayout title="۰۶ صفحات اصلی چندگانه">
         <img src={homeGymDefault} alt="homeGymDefault" className="max-md:w-[35rem] md:w-[21.5625rem] lg:w-[26.625rem]" />
-        <h5 className="text-center mt-4 font-medium text-xl">صفحه اصلی ۱ (صفحه پیش فرض)</h5>
+        <h5 className="text-center mt-4 font-medium text-xl">ص</h5>
       </BodyPageLayout>
     </>
   );
--- src/lib/context/HomePagesData/homeMultipages/homeMultipages.js
+++ src/lib/context/HomePagesData/homeMultipages/homeMultipages.js
@@ -1,5 +1,10 @@
+import homeGymDefault from "../../../../assets/images/homeMultipages/homeGymDefault.jpg";
 const homeMultiPagesData = [
-    
+  {
+    image: homeGymDefault,
+    title: "صفحه اصلی ۱ (صفحه پیش فرض)",
+    href: "https://rstheme.com/products/wordpress/gimnas/",
+  },
 ];
 
 export default homeMultiPagesData;
