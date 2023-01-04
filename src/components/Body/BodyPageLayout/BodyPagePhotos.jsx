92eac4317136423a3f483f6e62e182f23b2c1985
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 15:44:01 +0330

    Target is _blank

--- src/components/Body/BodyPageLayout/BodyPagePhotos.jsx
+++ src/components/Body/BodyPageLayout/BodyPagePhotos.jsx
@@ -2,7 +2,7 @@ import React, { memo } from "react";
 
 function BodyPagePhotos(props) {
   return (
-    <a href={props.href}>
+    <a target="_blank" href={props.href}>
       <img src={props.image} alt={props.title} className="max-md:w-[35rem] md:w-[21.5625rem] lg:w-[26.625rem]" />
       <h5 className="text-center mt-4 font-medium text-xl">{props.title}</h5>
     </a>
