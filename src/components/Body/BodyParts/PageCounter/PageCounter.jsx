89a461141c81a4f0e7f24be06414c896a151450f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 12:31:41 +0330

    Removed ref

--- src/components/Body/BodyParts/PageCounter/PageCounter.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounter.jsx
@@ -3,7 +3,7 @@ import PageCounterComponent from "./PageCounterComponent";
 
 function PageCounter() {
   return (
-    <section ref={ref} id="page-counter" className="bg-bgGray py-[5.625rem] flex justify-center items-center flex-wrap max-md:px-4 md:px-8 lg:px-[15%] max-lg:gap-20">
+    <section id="page-counter" className="bg-bgGray py-[5.625rem] flex justify-center items-center flex-wrap max-md:px-4 md:px-8 lg:px-[15%] max-lg:gap-20">
       <PageCounterComponent number="۱۲" text="تعداد صفحات اصلی" />
       <PageCounterComponent number="۰۶" text="صفحات اصلی چندگانه" />
       <PageCounterComponent number="۰۶" text="صفحات اصلی تکی" />
