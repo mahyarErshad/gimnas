e242013789540fc45fe81872b5bd2385b625ecf4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 15:43:19 +0330

    Import homeMultiPagesData

--- src/components/Body/MainBody.jsx
+++ src/components/Body/MainBody.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import homeMultiPagesData from "../../lib/context/HomePagesData/homeMultipages/homeMultipages";
 import BodyPageLayout from "./BodyPageLayout/BodyPageLayout";
 import PageCounter from "./BodyParts/PageCounter/PageCounter";
 
@@ -6,7 +7,7 @@ function MainBody() {
   return (
     <>
       <PageCounter />
-      <BodyPageLayout title="۰۶ صفحات اصلی چندگانه" />
+      <BodyPageLayout title="۰۶ صفحات اصلی چندگانه" data={homeMultiPagesData} />
     </>
   );
 }
