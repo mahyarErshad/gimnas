f8115b9d7642ea2ec742aac45359ec0149710e0c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 09:05:51 +0330

    Code has been refactored

--- src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function PageCounterComponent(props) {
-  return <div className="lg:p-8 md:p-6 max-md:p-4 max-md:w-full md:w-2/4 lg:w-1/4 flex flex-col justify-center items-center">
+  return <div className="lg:p-8 md:p-6 max-md:p-4 max-md:w-full md:w-2/4 lg:w-1/4 flex-col flex-full-center">
 
   </div>;
 }
