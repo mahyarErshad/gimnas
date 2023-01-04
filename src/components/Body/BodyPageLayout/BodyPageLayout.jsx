ae05d39b2c22a0fd5d88f6d9edaeb9a6c35c7a46
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 15:58:49 +0330

    Component is responsive

--- src/components/Body/BodyPageLayout/BodyPageLayout.jsx
+++ src/components/Body/BodyPageLayout/BodyPageLayout.jsx
@@ -4,11 +4,13 @@ import BodyPagePhotos from "./BodyPagePhotos";
 function BodyPageLayout(props) {
   const bgColor = props.bgColor === "bg-gray" ? "bg-bgGray" : "bg-white";
   return (
-    <section className={`${bgColor} flex-full-center flex-col pt-24 pb-14 max-md:px-4 md:px-8 lg:px-[10%] flex-wrap gap-4`}>
+    <section className={`${bgColor} flex-full-center flex-col pt-24 pb-14 max-md:px-4 md:px-8 lg:px-[10%]`}>
       <h3 className="mb-[4.6875rem] font-semibold text-3xl">{`[ ${props.title} ]`}</h3>
-      {props.data.map((item , index)=>{
-        return <BodyPagePhotos key={index} image={item.image} title={item.title} href={item.href} />
-      })}
+      <div className="flex-full-center flex-wrap w-full max-md:gap-4 md:gap-6 lg:gap-8">
+        {props.data.map((item, index) => {
+          return <BodyPagePhotos key={index} image={item.image} title={item.title} href={item.href} />;
+        })}
+      </div>
     </section>
   );
 }
