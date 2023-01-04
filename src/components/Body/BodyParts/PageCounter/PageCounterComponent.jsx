e24a0733c0cabc67f122fc836b3e2ea9fe0266f1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 09:36:59 +0330

    Rendering + symbol conditionally

--- src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
@@ -4,7 +4,7 @@ function PageCounterComponent(props) {
   return (
     <div className="max-md:w-full md:w-[40%] lg:w-1/4 flex-col flex-full-center gap-8">
       <h3 className="text-[5rem] font-bold text-primary m-0 leading-4">{props.number}</h3>
-      <h4 className="text-xl font-bold m-0">{props.text}</h4>
+      <h4 className="text-xl font-bold m-0">{props.text} {props.plus && <span className="text-inherit font-bold mx-0.5rem">+</span>} </h4>
     </div>
   );
 }
