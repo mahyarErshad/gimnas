aa4e3d4a49eb33ec937364280cea2f7106a6661a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 09:06:32 +0330

    Using props to render texts

--- src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 function PageCounterComponent(props) {
   return <div className="lg:p-8 md:p-6 max-md:p-4 max-md:w-full md:w-2/4 lg:w-1/4 flex-col flex-full-center">
-
+  <h3>{props.number}</h3>
+  <h4>{props.text}</h4>
   </div>;
 }
 
