b9071c951779cdd9ee3834e92b75ed8f49fc2794
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 09:15:54 +0330

    Component has been redesigned

--- src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function PageCounterComponent(props) {
   return (
-    <div className="lg:p-8 md:p-6 max-md:p-4 max-md:w-full md:w-2/4 lg:w-1/4 flex-col flex-full-center gap-10">
-      <h3 className="text-[5rem] font-bold text-primary">{props.number}</h3>
-      <h4 className="text-xl font-bold">{props.text}</h4>
+    <div className="lg:p-8 md:p-6 max-md:p-4 max-md:w-full md:w-2/4 lg:w-1/4 flex-col flex-full-center gap-8">
+      <h3 className="text-[5rem] font-bold text-primary m-0 leading-4">{props.number}</h3>
+      <h4 className="text-xl font-bold m-0">{props.text}</h4>
     </div>
   );
 }
