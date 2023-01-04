3671c12a3dfc8c80c42cf14e73e87f2792130d7c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 09:04:56 +0330

    Catching props

--- src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-function PageCounterComponent() {
+function PageCounterComponent(props) {
   return <div className="lg:p-8 md:p-6 max-md:p-4 max-md:w-full md:w-2/4 lg:w-1/4">
-    
+
   </div>;
 }
 
