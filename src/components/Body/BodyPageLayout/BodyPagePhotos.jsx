63689ce5aa20f87641c89f526818a429ba549769
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 15:56:16 +0330

    Added border radius

--- src/components/Body/BodyPageLayout/BodyPagePhotos.jsx
+++ src/components/Body/BodyPageLayout/BodyPagePhotos.jsx
@@ -2,7 +2,7 @@ import React, { memo } from "react";
 
 function BodyPagePhotos(props) {
   return (
-    <a target="_blank" href={props.href} className="custom-shadow">
+    <a target="_blank" href={props.href} className="custom-shadow pb-4 rounded-md">
       <img src={props.image} alt={props.title} className="max-md:w-[35rem] md:w-[21.5625rem] lg:w-[26.625rem]" />
       <h5 className="text-center mt-4 font-medium text-xl">{props.title}</h5>
     </a>
