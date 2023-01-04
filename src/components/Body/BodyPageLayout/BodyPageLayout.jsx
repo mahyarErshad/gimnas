d414d41ba517d22d0932524e4756cec7968ef061
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 15:21:41 +0330

    Flex wrap

--- src/components/Body/BodyPageLayout/BodyPageLayout.jsx
+++ src/components/Body/BodyPageLayout/BodyPageLayout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function BodyPageLayout(props) {
   const bgColor = props.bgColor === "bg-gray" ? "bg-bgGray" : "bg-white";
   return (
-    <section className={`${bgColor} flex-full-center flex-col pt-24 pb-14 max-md:px-4 md:px-8 lg:px-[10%]`}>
+    <section className={`${bgColor} flex-full-center flex-col pt-24 pb-14 max-md:px-4 md:px-8 lg:px-[10%] flex-wrap`}>
       <h3 className="mb-[4.6875rem] font-semibold text-3xl">{`[ ${props.title} ]`}</h3>
       {props.children}
     </section>
