99000b1f26ae7df8a6129c0f8682a654d4bd2705
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 09:19:47 +0330

    Using PageCounterComponent 4 times

--- src/components/Body/BodyParts/PageCounter/PageCounter.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounter.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import PageCounterComponent from "./PageCounterComponent";
 
 function PageCounter() {
-  return <section className="bg-bgGray py-[5.625rem] flex justify-center items-center flex-wrap">
-    <PageCounterComponent number="۱۲" text="تعداد صفحات اصلی" />
-  </section>;
+  return (
+    <section className="bg-bgGray py-[5.625rem] flex justify-center items-center flex-wrap">
+      <PageCounterComponent number="۱۲" text="تعداد صفحات اصلی" />
+      <PageCounterComponent number="۰۶" text="صفحات اصلی متعدد" />
+      <PageCounterComponent number="۰۶" text="صفحات اصلی تکی" />
+      <PageCounterComponent number="۴۶" text="صفحات داخلی" />
+    </section>
+  );
 }
 
 export default PageCounter;
