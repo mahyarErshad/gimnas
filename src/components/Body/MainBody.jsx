7e8f10988d82e01c6417c41d67c30348225d500b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 15:12:37 +0330

    Import BodyPageLayout

--- src/components/Body/MainBody.jsx
+++ src/components/Body/MainBody.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import BodyPageLayout from "./BodyPageLayout/BodyPageLayout";
 import PageCounter from "./BodyParts/PageCounter/PageCounter";
 
 function MainBody() {
   return (
     <>
       <PageCounter />
+      <BodyPageLayout >
+        
+      </BodyPageLayout>
     </>
   );
 }
