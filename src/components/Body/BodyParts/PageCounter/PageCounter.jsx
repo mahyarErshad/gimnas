9e647735b071e7a0d59a38bdc605d3b8d691aaee
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 09:35:16 +0330

    Changed the text

--- src/components/Body/BodyParts/PageCounter/PageCounter.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounter.jsx
@@ -5,7 +5,7 @@ function PageCounter() {
   return (
     <section className="bg-bgGray py-[5.625rem] flex justify-center items-center flex-wrap max-md:px-4 md:px-8 lg:px-[15%] max-lg:gap-20">
       <PageCounterComponent number="۱۲" text="تعداد صفحات اصلی" />
-      <PageCounterComponent number="۰۶" text="صفحات اصلی متعدد" />
+      <PageCounterComponent number="۰۶" text="صفحات اصلی چندگانه" />
       <PageCounterComponent number="۰۶" text="صفحات اصلی تکی" />
       <PageCounterComponent number="۴۶" text="صفحات داخلی" />
     </section>
