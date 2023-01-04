781d314d29449ddf49da326ddb4c10fe02e90e25
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 09:00:30 +0330

    Added 90px py

--- src/components/Body/BodyParts/PageCounter/PageCounter.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounter.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function PageCounter() {
-  return (
-    <section className='bg-bgGray'>PageCounter</section>
-  )
+  return <section className="bg-bgGray py-[5.625rem]">PageCounter</section>;
 }
 
-export default PageCounter
+export default PageCounter;
