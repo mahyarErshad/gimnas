375156939cc11a3cbc4b53b3368c738a330d8255
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 15:29:33 +0330

    h5 is done

--- src/components/Body/MainBody.jsx
+++ src/components/Body/MainBody.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import BodyPageLayout from "./BodyPageLayout/BodyPageLayout";
 import PageCounter from "./BodyParts/PageCounter/PageCounter";
-import homeGymDefault from "../../assets/images/homeMultipages/homeGymDefault.jpg"
+import homeGymDefault from "../../assets/images/homeMultipages/homeGymDefault.jpg";
 
 function MainBody() {
   return (
@@ -9,6 +9,7 @@ function MainBody() {
       <PageCounter />
       <BodyPageLayout title="۰۶ صفحات اصلی چندگانه">
         <img src={homeGymDefault} alt="homeGymDefault" className="max-md:w-[35rem] md:w-[21.5625rem] lg:w-[26.625rem]" />
+        <h5 className="text-center mt-4 font-medium text-xl">صفحه اصلی ۱ (صفحه پیش فرض)</h5>
       </BodyPageLayout>
     </>
   );
