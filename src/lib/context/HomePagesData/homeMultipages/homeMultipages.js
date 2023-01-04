50f8718b9f173eb676fb58964f30bca21e17362a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 15:53:16 +0330

    Added 2 new fields

--- src/lib/context/HomePagesData/homeMultipages/homeMultipages.js
+++ src/lib/context/HomePagesData/homeMultipages/homeMultipages.js
@@ -1,9 +1,21 @@
 import homeGymDefault from "../../../../assets/images/homeMultipages/homeGymDefault.jpg";
+import Home2 from "../../../../assets/images/homeMultipages/Home2.jpg";
+import Home3 from "../../../../assets/images/homeMultipages/Home3.jpg";
 const homeMultiPagesData = [
   {
     image: homeGymDefault,
     title: "صفحه اصلی ۱ (صفحه پیش فرض)",
     href: "https://rstheme.com/products/wordpress/gimnas/",
+  },
+  {
+    image: Home2,
+    title: "صفحه اصلی ۲ (باشگاه)",
+    href: "https://rstheme.com/products/wordpress/gimnas/home-two/",
+  },
+  {
+    image: Home3,
+    title: "صفحه اصلی ۳ (باشگاه)",
+    href: "https://rstheme.com/products/wordpress/gimnas/home-three/",
   },
 ];
 
