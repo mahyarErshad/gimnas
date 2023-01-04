d21c68acf9f33629216f2fb45405db54f0b2fc72
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 15:17:05 +0330

    Added a new image

--- src/components/Body/MainBody.jsx
+++ src/components/Body/MainBody.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import BodyPageLayout from "./BodyPageLayout/BodyPageLayout";
 import PageCounter from "./BodyParts/PageCounter/PageCounter";
+import homeGymDefault from "../../assets/images/homeMultipages/homeGymDefault.jpg"
 
 function MainBody() {
   return (
     <>
       <PageCounter />
-      <BodyPageLayout >
-        
+      <BodyPageLayout title="۰۶ صفحات اصلی چندگانه">
+        <img src={homeGymDefault} alt="homeGymDefault" />
       </BodyPageLayout>
     </>
   );
