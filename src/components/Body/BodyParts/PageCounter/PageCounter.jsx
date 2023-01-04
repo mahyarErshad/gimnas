cbcdfe4794d35e342e2e311cdf36fb24b9a7f977
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 09:54:35 +0330

    Installed gsap

--- src/components/Body/BodyParts/PageCounter/PageCounter.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounter.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import PageCounterComponent from "./PageCounterComponent";
 
 function PageCounter() {
+  
+
   return (
     <section className="bg-bgGray py-[5.625rem] flex justify-center items-center flex-wrap max-md:px-4 md:px-8 lg:px-[15%] max-lg:gap-20">
       <PageCounterComponent number="۱۲" text="تعداد صفحات اصلی" />
