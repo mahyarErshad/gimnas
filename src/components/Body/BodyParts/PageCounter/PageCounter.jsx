fa1be3fab8189b1052334bae5ca116fb18fa067a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 09:01:48 +0330

    Styles are given

--- src/components/Body/BodyParts/PageCounter/PageCounter.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function PageCounter() {
-  return <section className="bg-bgGray py-[5.625rem]">PageCounter</section>;
+  return <section className="bg-bgGray py-[5.625rem] flex justify-center items-center flex-wrap">PageCounter</section>;
 }
 
 export default PageCounter;
