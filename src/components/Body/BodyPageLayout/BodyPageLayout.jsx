c520c9a17cd498f02952397f1f3060df6cf9e3a1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 15:11:45 +0330

    BodyPageLayout has been done

--- src/components/Body/BodyPageLayout/BodyPageLayout.jsx
+++ src/components/Body/BodyPageLayout/BodyPageLayout.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
-function BodyPageLayout() {
+function BodyPageLayout(props) {
+  const bgColor = props.bgColor === "bg-gray" ? "bg-bgGray" : "bg-white";
   return (
-    <div>BodyPageLayout</div>
-  )
+    <section className={`${bgColor} flex-full-center pt-24 pb-14 max-md:px-4 md:px-8 lg:px-[10%]`}>
+      <h3 className="mb-[4.6875rem] font-semibold text-3xl">{`[ ${props.title} ]`}</h3>
+      {props.children}
+    </section>
+  );
 }
 
-export default BodyPageLayout
+export default BodyPageLayout;
