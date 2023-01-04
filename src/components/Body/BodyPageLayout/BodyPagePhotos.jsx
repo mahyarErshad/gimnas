9584d2519d049590b4bf82f8951fc644fb9d851a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-04 15:39:24 +0330

    Using props to become alive

--- src/components/Body/BodyPageLayout/BodyPagePhotos.jsx
+++ src/components/Body/BodyPageLayout/BodyPagePhotos.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function BodyPagePhotos(props) {
   return (
     <a>
-      <img src={homeGymDefault} alt="homeGymDefault" className="max-md:w-[35rem] md:w-[21.5625rem] lg:w-[26.625rem]" />
+      <img src={props.image} alt={props.title} className="max-md:w-[35rem] md:w-[21.5625rem] lg:w-[26.625rem]" />
       <h5 className="text-center mt-4 font-medium text-xl">{props.title}</h5>
     </a>
   );
