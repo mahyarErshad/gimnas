7528903a7aad5fbb12bf51ca94a4b0ecce319769
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-05 12:17:51 +0330

    MainFeatureCards are fully responsive

--- src/components/Body/MainFeatures/MainFeatureCards.jsx
+++ src/components/Body/MainFeatures/MainFeatureCards.jsx
@@ -2,10 +2,8 @@ import React from "react";
 
 function MainFeatureCards(props) {
   return (
-    <div className="shadow-md main-feature-card bg-white transition-all duration-500 rounded-[5px] p-6 mb-8 flex-full-center flex-col max-md:w-full md:w-2/4 lg:w-1/4 hover:bg-primary hover:text-white">
-      <div className="w-[5rem] h-[5rem] rounded-[50%] bg-white flex-full-center mb-4">
-      {props.children}
-      </div>
+    <div className="shadow-md main-feature-card bg-white transition-all duration-500 rounded-[5px] p-6 flex-full-center flex-col text-center max-md:w-[45%] md:w-[23%] hover:bg-primary hover:text-white">
+      <div className="w-[5rem] h-[5rem] rounded-[50%] bg-white flex-full-center mb-4">{props.children}</div>
       <h5 className="font-normal text-lg">{props.title}</h5>
     </div>
   );
--- src/components/Body/MainFeatures/MainFeatures.jsx
+++ src/components/Body/MainFeatures/MainFeatures.jsx
@@ -28,13 +28,15 @@ function MainFeatures() {
   return (
     <section className="bg-bgGray flex-full-center flex-col pt-24 pb-14 max-md:px-4 md:px-8 lg:px-[10%]">
       <h3 className="mb-[4.6875rem] font-semibold text-3xl">[ویژگی های اصلی]</h3>
-      {data.map((item, index) => {
-        return (
-          <MainFeatureCards key={index} title={item.title}>
-            <FontAwesomeIcon className="w-[3.125rem] h-[3.125rem]" icon={item.icon} />
-          </MainFeatureCards>
-        );
-      })}
+      <div className="w-full flex-full-center gap-4 flex-wrap">
+        {data.map((item, index) => {
+          return (
+            <MainFeatureCards key={index} title={item.title}>
+              <FontAwesomeIcon className="w-[3.125rem] h-[3.125rem]" icon={item.icon} />
+            </MainFeatureCards>
+          );
+        })}
+      </div>
     </section>
   );
 }
