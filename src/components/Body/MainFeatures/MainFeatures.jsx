df99653764eb29f41a26665b0cd6a3db414832c7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-05 10:29:50 +0330

    Imported MainFeatureCards

--- src/components/Body/MainFeatures/MainFeatures.jsx
+++ src/components/Body/MainFeatures/MainFeatures.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import MainFeatureCards from "./MainFeatureCards";
 
 function MainFeatures() {
   return (
     <section className="bg-bgGray flex-full-center flex-col pt-24 pb-14 max-md:px-4 md:px-8 lg:px-[10%]">
       <h3 className="mb-[4.6875rem] font-semibold text-3xl">[ویژگی های اصلی]</h3>
-      
+      <MainFeatureCards />
     </section>
   );
 }
