19a4c0623064ff7f327aa6fb6df74e2b0e7f6470
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-05 10:21:32 +0330

    Giving styles to h5

--- src/components/Body/MainFeatures/MainFeatureCards.jsx
+++ src/components/Body/MainFeatures/MainFeatureCards.jsx
@@ -5,7 +5,7 @@ function MainFeatureCards(props) {
   return (
     <div className="custom-shadow bg-white transition-all rounded-[5px] p-6 mb-8 flex-full-center flex-col max-md:w-full md:w-2/4 lg:w-1/4">
       <img className="w-[3.125rem] h-[3.125rem] mb-[1.5625rem]" src={props.image} alt={props.title} />
-      <h5>{props.title}</h5>
+      <h5 className="font-normal text-lg">{props.title}</h5>
     </div>
   );
 }
