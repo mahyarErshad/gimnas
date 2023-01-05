5fcbaaaa0ee9de0fde8736d0da0c61550b4094eb
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-05 10:19:27 +0330

    Give classname to the image

--- src/components/Body/MainFeatures/MainFeatureCards.jsx
+++ src/components/Body/MainFeatures/MainFeatureCards.jsx
@@ -1,12 +1,12 @@
-import PreviousMap from 'postcss/lib/previous-map'
-import React from 'react'
+import PreviousMap from "postcss/lib/previous-map";
+import React from "react";
 
 function MainFeatureCards(props) {
   return (
-    <div className='custom-shadow bg-white transition-all rounded-[5px] p-6 mb-8 flex-full-center flex-col'>
-    <img src={props.image} alt={props.title} />
+    <div className="custom-shadow bg-white transition-all rounded-[5px] p-6 mb-8 flex-full-center flex-col">
+      <img className="w-[3.125rem] h-[3.125rem] mb-[1.5625rem]" src={props.image} alt={props.title} />
     </div>
-  )
+  );
 }
 
-export default MainFeatureCards
+export default MainFeatureCards;
