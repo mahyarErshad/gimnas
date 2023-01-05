2b15f312d677effcf154cb94c66fd5cd7126a95e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-05 10:22:37 +0330

    Added on hover styles

--- src/components/Body/MainFeatures/MainFeatureCards.jsx
+++ src/components/Body/MainFeatures/MainFeatureCards.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 function MainFeatureCards(props) {
   return (
-    <div className="custom-shadow bg-white transition-all rounded-[5px] p-6 mb-8 flex-full-center flex-col max-md:w-full md:w-2/4 lg:w-1/4">
-      <img className="w-[3.125rem] h-[3.125rem] mb-[1.5625rem]" src={props.image} alt={props.title} />
-      <h5 className="font-normal text-lg">{props.title}</h5>
+    <div className="custom-shadow bg-white transition-all rounded-[5px] p-6 mb-8 flex-full-center flex-col max-md:w-full md:w-2/4 lg:w-1/4 hover:text-primary">
+      <img className="w-[3.125rem] h-[3.125rem] mb-[1.5625rem] hover:text-primary" src={props.image} alt={props.title} />
+      <h5 className="font-normal text-lg text-inherit">{props.title}</h5>
     </div>
   );
 }
