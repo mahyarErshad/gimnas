d65fa340415906796b73c211b42f7ad15704e740
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-05 09:57:12 +0330

    Layout has been Created

--- src/components/Body/FullyResponsive/FullyResponsive.jsx
+++ src/components/Body/FullyResponsive/FullyResponsive.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
-function FullyResponsive() {
+function FullyResponsive(props) {
+  const bgColor = props.bgColor === "bg-gray" ? "bg-bgGray" : "bg-white";
   return (
-    <div>FullyResponsive</div>
-  )
+    <section className={`${bgColor} flex-full-center flex-col pt-24 pb-14 max-md:px-4 md:px-8 lg:px-[10%]`}>
+      <h3 className="mb-[4.6875rem] font-semibold text-3xl">{`[ ${props.title} ]`}</h3>
+    </section>
+  );
 }
 
-export default FullyResponsive
+export default FullyResponsive;
