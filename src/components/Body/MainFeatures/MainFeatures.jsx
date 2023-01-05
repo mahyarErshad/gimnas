0247fc53aa9e444f18cc13aa402d4d1e6fa3b608
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-05 10:27:19 +0330

    Installed font awesome

--- src/components/Body/MainFeatures/MainFeatures.jsx
+++ src/components/Body/MainFeatures/MainFeatures.jsx
@@ -4,6 +4,7 @@ function MainFeatures() {
   return (
     <section className="bg-bgGray flex-full-center flex-col pt-24 pb-14 max-md:px-4 md:px-8 lg:px-[10%]">
       <h3 className="mb-[4.6875rem] font-semibold text-3xl">[ویژگی های اصلی]</h3>
+      
     </section>
   );
 }
