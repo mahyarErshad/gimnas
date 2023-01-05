1bd7dc6ba126d457302f2899372fc285dd28e7ab
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-05 10:08:28 +0330

    Changed the bg color

--- src/components/Body/FullyResponsive/FullyResponsive.jsx
+++ src/components/Body/FullyResponsive/FullyResponsive.jsx
@@ -4,7 +4,7 @@ import fullyResponsive from "../../../assets/images/fullyResponsive.jpg"
 function FullyResponsive() {
 
   return (
-    <section className="bg-bgGray flex-full-center flex-col pt-24 pb-14 px-4">
+    <section className="bg-white flex-full-center flex-col pt-24 pb-14 px-4">
       <h3 className="mb-[4.6875rem] font-semibold text-3xl">[سازگار با انواع نمایشگرها]</h3>
       <img className="w-full" src={fullyResponsive} alt="Fully Responsive" />
     </section>
