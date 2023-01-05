140b09316c64415018249e046d2a459dd80966cf
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-05 10:16:37 +0330

    Wrapper has styled

--- src/components/Body/MainFeatures/MainFeatureCards.jsx
+++ src/components/Body/MainFeatures/MainFeatureCards.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function MainFeatureCards() {
   return (
-    <div>MainFeatureCards</div>
+    <div className='custom-shadow bg-white transition-all rounded-[5px] p-6 mb-8'>
+
+    </div>
   )
 }
 
