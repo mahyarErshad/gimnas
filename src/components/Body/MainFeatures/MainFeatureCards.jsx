42d6eba4e1a19716436b8d08b5b43f59a0398e8b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-05 10:18:10 +0330

    Added image

--- src/components/Body/MainFeatures/MainFeatureCards.jsx
+++ src/components/Body/MainFeatures/MainFeatureCards.jsx
@@ -1,9 +1,10 @@
+import PreviousMap from 'postcss/lib/previous-map'
 import React from 'react'
 
-function MainFeatureCards() {
+function MainFeatureCards(props) {
   return (
-    <div className='custom-shadow bg-white transition-all rounded-[5px] p-6 mb-8'>
-
+    <div className='custom-shadow bg-white transition-all rounded-[5px] p-6 mb-8 flex-full-center flex-col'>
+    <img src={props.image} alt={props.title} />
     </div>
   )
 }
