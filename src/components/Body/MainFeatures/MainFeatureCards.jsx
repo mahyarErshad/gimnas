e0a7053bcc0be9cb30a7c69690c98a2473d941e6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-05 10:20:12 +0330

    Card width is responsive

--- src/components/Body/MainFeatures/MainFeatureCards.jsx
+++ src/components/Body/MainFeatures/MainFeatureCards.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function MainFeatureCards(props) {
   return (
-    <div className="custom-shadow bg-white transition-all rounded-[5px] p-6 mb-8 flex-full-center flex-col">
+    <div className="custom-shadow bg-white transition-all rounded-[5px] p-6 mb-8 flex-full-center flex-col max-md:w-full md:w-2/4 lg:w-1/4">
       <img className="w-[3.125rem] h-[3.125rem] mb-[1.5625rem]" src={props.image} alt={props.title} />
     </div>
   );
