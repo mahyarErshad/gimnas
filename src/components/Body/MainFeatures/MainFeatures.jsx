1f6ea4f9fbfb459a54c2e51bf0a0f56795803a40
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-05 12:02:30 +0330

    Added the first row

--- src/components/Body/MainFeatures/MainFeatures.jsx
+++ src/components/Body/MainFeatures/MainFeatures.jsx
@@ -1,15 +1,40 @@
 import { faWordpress } from "@fortawesome/free-brands-svg-icons";
+import { faNewspaper } from "@fortawesome/free-regular-svg-icons";
+import { faTabletScreenButton } from "@fortawesome/free-solid-svg-icons";
+import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import MainFeatureCards from "./MainFeatureCards";
 
 function MainFeatures() {
+  const data = [
+    {
+      title: "وردپرس",
+      icon: faWordpress,
+    },
+    {
+      title: "۱۲ دمو",
+      icon: faNewspaper,
+    },
+    {
+      title: "کاملا واکنشگر",
+      icon: faTabletScreenButton,
+    },
+    {
+      title: "اسلایدر روولوشن",
+      icon: faArrowsRotate,
+    },
+  ];
   return (
     <section className="bg-bgGray flex-full-center flex-col pt-24 pb-14 max-md:px-4 md:px-8 lg:px-[10%]">
       <h3 className="mb-[4.6875rem] font-semibold text-3xl">[ویژگی های اصلی]</h3>
-      <MainFeatureCards title="وردپرس">
-        <FontAwesomeIcon className="w-[3.125rem] h-[3.125rem]" icon={faWordpress} />
-      </MainFeatureCards>
+      {data.map((item, index) => {
+        return (
+          <MainFeatureCards key={index} title={item.title}>
+            <FontAwesomeIcon className="w-[3.125rem] h-[3.125rem]" icon={item.icon} />
+          </MainFeatureCards>
+        );
+      })}
     </section>
   );
 }
