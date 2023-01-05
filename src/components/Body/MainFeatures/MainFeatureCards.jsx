e9170df5d9cff8c9ebdc5c775769e8875421d43f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-05 11:19:11 +0330

    Cards are designed and ready

--- src/components/Body/MainFeatures/MainFeatureCards.jsx
+++ src/components/Body/MainFeatures/MainFeatureCards.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { faWordpress } from "@fortawesome/free-brands-svg-icons";
 
 function MainFeatureCards(props) {
   return (
-    <div className="custom-shadow bg-white transition-all rounded-[5px] p-6 mb-8 flex-full-center flex-col max-md:w-full md:w-2/4 lg:w-1/4 hover:text-primary">
-      {/* <img className="w-[3.125rem] h-[3.125rem] mb-[1.5625rem] hover:text-primary" src={props.image} alt={props.title} /> */}
-      <FontAwesomeIcon icon={faCoffee} />
-      <h5 className="font-normal text-lg text-inherit">{props.title}</h5>
+    <div className="shadow-md main-feature-card bg-white transition-all duration-500 rounded-[5px] p-6 mb-8 flex-full-center flex-col max-md:w-full md:w-2/4 lg:w-1/4 hover:bg-primary hover:text-white">
+      <div className="w-[5rem] h-[5rem] rounded-[50%] bg-white flex-full-center mb-4">
+        <FontAwesomeIcon className="w-[3.125rem] h-[3.125rem]" icon={faWordpress} />
+      </div>
+      <h5 className="font-normal text-lg">وردپرس</h5>
     </div>
   );
 }
