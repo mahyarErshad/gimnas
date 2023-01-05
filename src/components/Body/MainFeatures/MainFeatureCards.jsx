5030ea3f8cca54538cfcee33afdfde5420d77414
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-05 11:38:47 +0330

    Card is designed and styled

--- src/components/Body/MainFeatures/MainFeatureCards.jsx
+++ src/components/Body/MainFeatures/MainFeatureCards.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWordpress } from "@fortawesome/free-brands-svg-icons";
 
 function MainFeatureCards(props) {
   return (
     <div className="shadow-md main-feature-card bg-white transition-all duration-500 rounded-[5px] p-6 mb-8 flex-full-center flex-col max-md:w-full md:w-2/4 lg:w-1/4 hover:bg-primary hover:text-white">
       <div className="w-[5rem] h-[5rem] rounded-[50%] bg-white flex-full-center mb-4">
-        <FontAwesomeIcon className="w-[3.125rem] h-[3.125rem]" icon={faWordpress} />
+      {props.children}
       </div>
-      <h5 className="font-normal text-lg">وردپرس</h5>
+      <h5 className="font-normal text-lg">{props.title}</h5>
     </div>
   );
 }
--- src/components/Body/MainFeatures/MainFeatures.jsx
+++ src/components/Body/MainFeatures/MainFeatures.jsx
@@ -1,3 +1,5 @@
+import { faWordpress } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import MainFeatureCards from "./MainFeatureCards";
 
@@ -5,7 +7,9 @@ function MainFeatures() {
   return (
     <section className="bg-bgGray flex-full-center flex-col pt-24 pb-14 max-md:px-4 md:px-8 lg:px-[10%]">
       <h3 className="mb-[4.6875rem] font-semibold text-3xl">[ویژگی های اصلی]</h3>
-      <MainFeatureCards />
+      <MainFeatureCards title="وردپرس">
+        <FontAwesomeIcon className="w-[3.125rem] h-[3.125rem]" icon={faWordpress} />
+      </MainFeatureCards>
     </section>
   );
 }
