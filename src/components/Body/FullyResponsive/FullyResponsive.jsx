e52ac9bf8c3c45582957a284ef0ddd26f3d09785
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-05 10:01:24 +0330

    Added a new photo

--- src/components/Body/FullyResponsive/FullyResponsive.jsx
+++ src/components/Body/FullyResponsive/FullyResponsive.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-function FullyResponsive(props) {
-  const bgColor = props.bgColor === "bg-gray" ? "bg-bgGray" : "bg-white";
+function FullyResponsive() {
+
   return (
-    <section className={`${bgColor} flex-full-center flex-col pt-24 pb-14 px-4`}>
-      <h3 className="mb-[4.6875rem] font-semibold text-3xl">{`[ ${props.title} ]`}</h3>
+    <section className="bg-bgGray flex-full-center flex-col pt-24 pb-14 px-4">
+      <h3 className="mb-[4.6875rem] font-semibold text-3xl">[سازگار با انواع نمایشگرها]</h3>
+      <img src="" alt="Fully Responsive" />
     </section>
   );
 }
