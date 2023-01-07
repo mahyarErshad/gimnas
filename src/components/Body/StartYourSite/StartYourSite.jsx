7f3776fb9655f5b8d6bc8b9e38cd0fb5bd3066ef
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-07 10:06:05 +0330

    Added href to #demos

--- src/components/Body/StartYourSite/StartYourSite.jsx
+++ src/components/Body/StartYourSite/StartYourSite.jsx
@@ -7,7 +7,7 @@ function StartYourSite() {
       <h2 className="max-md:text-xl md:text-3xl text-center mb-4 font-normal">شروع به طراحی سایت مورد نظر خود کنید</h2>
       <h3 className="max-md:text-xl md:text-3xl text-center font-bold max-md:mb-6 md:mb-8">با جیمناس - تم باشگاهی وردپرس</h3>
       <div className="flex-full-center gap-4 flex-wrap">
-        <CustomButton title="مشاهده دموها" />
+        <CustomButton href="#demos" title="مشاهده دموها" />
         <CustomButton target="_blank" href="https://www.rtl-theme.com/gimnas-sport-worpress-theme/" title="خرید نسخه حرفه ایی" />
       </div>
     </section>
