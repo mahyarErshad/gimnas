f7d535fcacd895e77d78c6ab78915e76e4a06992
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-07 09:16:22 +0330

    Added the image

--- src/components/Body/WooCommerce/WooCommerce.jsx
+++ src/components/Body/WooCommerce/WooCommerce.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import wooCommerce from "../../../assets/images/wooCommerce.png";
 
 function WooCommerce() {
   return (
     <section className="bg-white flex-full-center flex-col pt-24 pb-14 px-4">
-        
+      <img src={wooCommerce} alt="WooCommerce" />
     </section>
-  )
+  );
 }
 
-export default WooCommerce
+export default WooCommerce;
