554f771357caa38b7b60990abbd7072b451f171a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-07 10:00:07 +0330

    Footer has done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,9 @@ import gimnas from "../../assets/images/logo-black.png";
 
 function Footer() {
   return (
-    <footer className="flex-full-center bg-white py-8 px-4">
-      <img src={gimnas} alt="Gimnas Logo" />
+    <footer className="flex-full-center flex-col bg-white py-9 px-4">
+      <img className="w-[12.5rem] h-[2.5rem] mb-8" src={gimnas} alt="Gimnas Logo" />
+      <p className="text-[#999] text-lg">&copy; طراحی شده توسط گروه طراحی وب راهینو.</p>
     </footer>
   );
 }
