c6c3c877f9020b45cfcd9b884591ad7237131401
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-07 09:29:29 +0330

    Changed the displaying text

--- src/components/Body/StartYourSite/StartYourSite.jsx
+++ src/components/Body/StartYourSite/StartYourSite.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function StartYourSite() {
   return (
     <section className="bg-bgGray flex-full-center flex-col py-24 px-4">
-      <h2 className="max-md:text-xl md:text-[2.5rem] text-center font-light">شروع به ساخت مد نظر خود کنید</h2>
-      <h3 className="max-md:text-xl md:text-[2.5rem] text-center font-bold">با جیمناس - تم باشگاهی وردپرس</h3>
+      <h2 className="max-md:text-xl md:text-3xl text-center font-normal">شروع به طراحی سایت مورد نظر خود کنید</h2>
+      <h3 className="max-md:text-xl md:text-3xl text-center font-bold">با جیمناس - تم باشگاهی وردپرس</h3>
     </section>
   );
 }
