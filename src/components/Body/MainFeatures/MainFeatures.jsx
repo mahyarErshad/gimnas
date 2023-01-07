7abe1267b01c809e2b7906e360d9a720134de518
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-07 09:08:12 +0330

    Items are added

--- src/components/Body/MainFeatures/MainFeatures.jsx
+++ src/components/Body/MainFeatures/MainFeatures.jsx
@@ -1,7 +1,15 @@
-import { faWordpress } from "@fortawesome/free-brands-svg-icons";
-import { faNewspaper } from "@fortawesome/free-regular-svg-icons";
+import { faChrome, faWordpress } from "@fortawesome/free-brands-svg-icons";
+import { faFileCode, faLifeRing, faNewspaper, faThumbsUp } from "@fortawesome/free-regular-svg-icons";
+import { faRectangleList } from "@fortawesome/free-regular-svg-icons";
 import { faTabletScreenButton } from "@fortawesome/free-solid-svg-icons";
 import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { faArrowsUpDownLeftRight } from "@fortawesome/free-solid-svg-icons";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { faEyeDropper } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { memo } from "react";
 import MainFeatureCards from "./MainFeatureCards";
@@ -23,6 +31,54 @@ function MainFeatures() {
     {
       title: "اسلایدر روولوشن",
       icon: faArrowsRotate,
+    },
+    {
+      title: "ویژوال کامپوزر",
+      icon: faArrowsUpDownLeftRight,
+    },
+    {
+      title: "پنل ادمین قدرتمند",
+      icon: faGear,
+    },
+    {
+      title: "عناصر پویا",
+      icon: faPaperPlane,
+    },
+    {
+      title: "بینهایت رنگ",
+      icon: faEyeDropper,
+    },
+    {
+      title: "سازگار با سئو",
+      icon: faMagnifyingGlass,
+    },
+    {
+      title: "سازگار با میل چیمپ",
+      icon: faEnvelope,
+    },
+    {
+      title: "چندین قالب برای وبلاگ",
+      icon: faRectangleList,
+    },
+    {
+      title: "کد نویسی تمیز",
+      icon: faCode,
+    },
+    {
+      title: "سازگار با مرورگرها",
+      icon: faChrome,
+    },
+    {
+      title: "فریم ورک ریداکس",
+      icon: faThumbsUp,
+    },
+    {
+      title: "مستندسازی عالی",
+      icon: faFileCode,
+    },
+    {
+      title: "پشتیبانی ۲۴/۷",
+      icon: faLifeRing,
     },
   ];
   return (
