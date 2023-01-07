a5b010a4af7f8d78d4720c049fef13ca72c5cbc6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-07 09:37:44 +0330

    added 2 new buttons

--- src/components/Body/StartYourSite/StartYourSite.jsx
+++ src/components/Body/StartYourSite/StartYourSite.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import CustomButton from "../../Utils/Buttons/CustomButton/CustomButton";
 
 function StartYourSite() {
   return (
     <section className="bg-bgGray flex-full-center flex-col py-24 px-4">
-      <h2 className="max-md:text-xl md:text-3xl text-center font-normal">شروع به طراحی سایت مورد نظر خود کنید</h2>
-      <h3 className="max-md:text-xl md:text-3xl text-center font-bold">با جیمناس - تم باشگاهی وردپرس</h3>
+      <h2 className="max-md:text-xl md:text-3xl text-center max-md:mb-4 md:mb-6 font-normal">شروع به طراحی سایت مورد نظر خود کنید</h2>
+      <h3 className="max-md:text-xl md:text-3xl text-center font-bold max-md:mb-6 md:mb-8">با جیمناس - تم باشگاهی وردپرس</h3>
+      <div className="flex-full-center gap-4 flex-wrap">
+        <CustomButton title="مشاهده دموها" />
+        <CustomButton title="خرید نسخه حرفه ایی" />
+      </div>
     </section>
   );
 }
