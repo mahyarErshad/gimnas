8253ea7b9327d409c771d2d21141e1bf35346ffa
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-07 09:27:20 +0330

    Added h2 and h3

--- src/components/Body/StartYourSite/StartYourSite.jsx
+++ src/components/Body/StartYourSite/StartYourSite.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function StartYourSite() {
   return (
     <section className="bg-bgGray flex-full-center flex-col py-24 px-4">
-
+      <h2 className="max-md:text-xl md:text-[2.5rem] text-center font-light">شروع به ساخت مد نظر خود کنید</h2>
+      <h3 className="max-md:text-xl md:text-[2.5rem] text-center font-bold">با جیمناس - تم باشگاهی وردپرس</h3>
     </section>
-  )
+  );
 }
 
-export default StartYourSite
+export default StartYourSite;
