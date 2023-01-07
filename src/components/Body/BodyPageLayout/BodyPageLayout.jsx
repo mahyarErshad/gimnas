7708644f36217f753af872d791ba78561df0b938
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-07 10:03:59 +0330

    Added href for srolling to the demos

--- src/components/Body/BodyPageLayout/BodyPageLayout.jsx
+++ src/components/Body/BodyPageLayout/BodyPageLayout.jsx
@@ -4,7 +4,7 @@ import BodyPagePhotos from "./BodyPagePhotos";
 function BodyPageLayout(props) {
   const bgColor = props.bgColor === "bg-gray" ? "bg-bgGray" : "bg-white";
   return (
-    <section className={`${bgColor} flex-full-center flex-col pt-24 pb-14 max-md:px-4 md:px-8 lg:px-[10%]`}>
+    <section id="demos" className={`${bgColor} flex-full-center flex-col pt-24 pb-14 max-md:px-4 md:px-8 lg:px-[10%]`}>
       <h3 className="mb-[4.6875rem] font-semibold text-3xl">{`[ ${props.title} ]`}</h3>
       <div className="flex-full-center flex-wrap w-full max-md:gap-4 md:gap-6 lg:gap-8">
         {props.data.map((item, index) => {
--- src/components/Header/HeaderTextSection/HeaderTextSection.jsx
+++ src/components/Header/HeaderTextSection/HeaderTextSection.jsx
@@ -7,7 +7,7 @@ function HeaderTextSection() {
   return (
     <div id="HeaderTextSection" onMouseMove={animatedCircles} className="text-white w-full h-full pt-44 overflow-hidden flex justify-start flex-col items-center max-lg:gap-6 lg:gap-8">
       <HeaderTexts />
-      <InternalNavigationButton />
+      <InternalNavigationButton href="#demos" />
     </div>
   );
 }
