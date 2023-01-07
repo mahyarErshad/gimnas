8c662c55782f79366bfcfd0576366318825d911c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-07 09:44:35 +0330

    Target can be passed via props

--- src/components/Utils/Buttons/CustomButton/CustomButton.jsx
+++ src/components/Utils/Buttons/CustomButton/CustomButton.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 function CustomButton(props) {
   const href = props.href || "#";
+  const target = props.target || "";
   return (
-    <a href={href} target={href !== "#" ? "_blank" : ""}>
+    <a href={href} target={target}>
       <button className="bg-primary shadow-sm text-white z-40 transition-all hover:opacity-90 max-md:py-3 md:py-4 max-md:px-6 md:px-8 max-md:text-base md:text-lg lg:text-xl font-medium rounded-[1.875rem] text-center">{props.title}</button>
     </a>
   );
