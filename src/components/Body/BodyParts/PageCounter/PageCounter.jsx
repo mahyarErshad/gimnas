95542d659692e5a69198990b550385cb86fce20b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-08 09:51:37 +0330

    Counter is working on scroll without any bugs

--- src/components/Body/BodyParts/PageCounter/PageCounter.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounter.jsx
@@ -1,18 +1,26 @@
-import React, { memo, useContext } from "react";
+import React, { memo, useContext, useState } from "react";
 import PageCounterComponent from "./PageCounterComponent";
 import HeaderSliderButton from "../../../Utils/Buttons/HeaderSliderButton/HeaderSliderButton"
 import ModalContext from "../../../../lib/context/ModalContext";
+import ScrollTrigger from "react-scroll-trigger";
+
 
 function PageCounter() {
   const modalContext = useContext(ModalContext);
+  const [counterStart, setCounterStart] = useState(false);
+  
   return (
-    <section id="page-counter" className="bg-white relative z-10 py-[5.625rem] flex justify-center items-center flex-wrap max-md:px-4 md:px-8 lg:px-[15%] max-lg:gap-20">
+    <section id="page-counter" className="bg-white relative z-10 py-[5.625rem]">
       <HeaderSliderButton onClick={() => {modalContext.setIsModalOpen(true)}} title="پشتیبانی" image="https://img.icons8.com/windows/32/FFFFFF/help.png" top="top-[75%]" />
       <HeaderSliderButton onClick={() => window.open("https://www.rtl-theme.com/gimnas-sport-worpress-theme/", "_blank")} title="خرید قالب" image="https://img.icons8.com/external-creatype-outline-colourcreatype/28/FFFFFF/external-basket-miscellaneous-user-interface-v2-creatype-outline-colourcreatype.png" top="top-[81.5%]" />
-      <PageCounterComponent number={12} text="تعداد صفحات اصلی" />
-      <PageCounterComponent number={6} text="صفحات اصلی چندگانه" />
-      <PageCounterComponent number={6} text="صفحات اصلی تکی" />
-      <PageCounterComponent plus number={46} text="صفحات داخلی" />
+    <ScrollTrigger onEnter={() => setCounterStart(true)} onExit={() => setCounterStart(false)}>
+    <div className="flex-full-center max-md:px-4 md:px-8 flex-wrap w-full h-full lg:px-[15%] max-lg:gap-20">
+      <PageCounterComponent counterStart={counterStart} number={12} text="تعداد صفحات اصلی" />
+      <PageCounterComponent counterStart={counterStart} number={6} text="صفحات اصلی چندگانه" />
+      <PageCounterComponent counterStart={counterStart} number={6} text="صفحات اصلی تکی" />
+      <PageCounterComponent counterStart={counterStart} plus number={46} text="صفحات داخلی" />
+    </div>
+    </ScrollTrigger>
     </section>
   );
 }
--- src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
@@ -1,20 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import CountUp from "react-countup";
-import ScrollTrigger from "react-scroll-trigger";
 
 function PageCounterComponent(props) {
-  const [counterStart, setCounterStart] = useState(false);
   return (
-    <ScrollTrigger onEnter={() => setCounterStart(true)} onExit={() => setCounterStart(false)}>
-      <div className="max-md:w-full md:w-[40%] lg:w-1/4 flex-col flex-full-center gap-8">
-        <h3 className="text-[5rem] font-bold text-primary m-0 leading-4 flex js-count-up">
-          {counterStart && <CountUp start={0} end={props.number} duration={2} delay={0} />}
-
-          {props.plus && <span className="text-inherit font-bold">+</span>}
-        </h3>
-        <h4 className="text-xl font-bold m-0">{props.text}</h4>
-      </div>
-    </ScrollTrigger>
+    <div className="max-md:w-full md:w-[40%] lg:w-1/4 flex-col flex-full-center gap-8">
+      <h3 className="text-[5rem] font-bold text-primary m-0 leading-4 flex js-count-up">
+        {props.counterStart && <CountUp start={0} end={props.number} duration={2} delay={0} />}
+        {props.plus && <span className="text-inherit font-bold">+</span>}
+      </h3>
+      <h4 className="text-xl font-bold m-0">{props.text}</h4>
+    </div>
   );
 }
 
