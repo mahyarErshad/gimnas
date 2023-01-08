8f2a73398ffc0b60a94f4327406e7b9742b93777
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-08 08:27:02 +0330

    Copied the codes

--- src/components/Utils/Buttons/GoToTopButton/GoToTopButton.jsx
+++ src/components/Utils/Buttons/GoToTopButton/GoToTopButton.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 function GoToTopButton() {
+  const [scrollHeight, setScrollHeight] = useState(false);
+  const root = document.getElementById("root");
+  function handleScroll() {
+    root.scroll({
+      top: 0,
+    });
+  }
+  useEffect(() => {
+    root.addEventListener("scroll", () => {
+      if (root.scrollTop > 1000) {
+        setScrollHeight(true);
+      } else {
+        setScrollHeight(false);
+      }
+    });
+  }, [scrollHeight]);
+
   return (
-    <div>GoToTopButton</div>
-  )
+    <>
+      {scrollHeight && (
+        <button onClick={handleScroll} className={`cursor-pointer rounded-[50%] p-2 flex justify-center items-center border border-white fixed bottom-[5%] left-[5%] bg-purple transition-all hover:bg-[#1f3fca] text-white ${scrollHeight ? "goToTopButton" : ""}`}>
+          <span className="material-symbols-outlined text-inherit">arrow_upward</span>
+        </button>
+      )}
+    </>
+  );
 }
 
-export default GoToTopButton
+export default GoToTopButton;
