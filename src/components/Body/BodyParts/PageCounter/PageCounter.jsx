42200944e3280accff88e12ea2c7f401b1529d53
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-08 09:56:11 +0330

    Fixed a responsive bug

--- src/components/Body/BodyParts/PageCounter/PageCounter.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounter.jsx
@@ -14,7 +14,7 @@ function PageCounter() {
       <HeaderSliderButton onClick={() => {modalContext.setIsModalOpen(true)}} title="پشتیبانی" image="https://img.icons8.com/windows/32/FFFFFF/help.png" top="top-[75%]" />
       <HeaderSliderButton onClick={() => window.open("https://www.rtl-theme.com/gimnas-sport-worpress-theme/", "_blank")} title="خرید قالب" image="https://img.icons8.com/external-creatype-outline-colourcreatype/28/FFFFFF/external-basket-miscellaneous-user-interface-v2-creatype-outline-colourcreatype.png" top="top-[81.5%]" />
     <ScrollTrigger onEnter={() => setCounterStart(true)} onExit={() => setCounterStart(false)}>
-    <div className="flex-full-center max-md:px-4 md:px-8 flex-wrap w-full h-full lg:px-[15%] max-lg:gap-20">
+    <div className="flex justify-center items-start max-md:px-4 md:px-8 flex-wrap w-full h-full lg:px-[15%] max-lg:gap-20">
       <PageCounterComponent counterStart={counterStart} number={12} text="تعداد صفحات اصلی" />
       <PageCounterComponent counterStart={counterStart} number={6} text="صفحات اصلی چندگانه" />
       <PageCounterComponent counterStart={counterStart} number={6} text="صفحات اصلی تکی" />
