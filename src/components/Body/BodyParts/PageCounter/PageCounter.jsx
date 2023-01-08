de01dfe2d7aeabdc48312ec3ecd1b44c2c909ca5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-08 09:04:22 +0330

    Position is relative

--- src/components/Body/BodyParts/PageCounter/PageCounter.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounter.jsx
@@ -6,7 +6,7 @@ import ModalContext from "../../../../lib/context/ModalContext";
 function PageCounter() {
   const modalContext = useContext(ModalContext);
   return (
-    <section id="page-counter" className="bg-white z-10 py-[5.625rem] flex justify-center items-center flex-wrap max-md:px-4 md:px-8 lg:px-[15%] max-lg:gap-20">
+    <section id="page-counter" className="bg-white relative z-10 py-[5.625rem] flex justify-center items-center flex-wrap max-md:px-4 md:px-8 lg:px-[15%] max-lg:gap-20">
       <HeaderSliderButton onClick={() => {modalContext.setIsModalOpen(true)}} title="پشتیبانی" image="https://img.icons8.com/windows/32/FFFFFF/help.png" top="top-[75%]" />
       <HeaderSliderButton onClick={() => window.open("https://www.rtl-theme.com/gimnas-sport-worpress-theme/", "_blank")} title="خرید قالب" image="https://img.icons8.com/external-creatype-outline-colourcreatype/28/FFFFFF/external-basket-miscellaneous-user-interface-v2-creatype-outline-colourcreatype.png" top="top-[81.5%]" />
       <PageCounterComponent number="۱۲" text="تعداد صفحات اصلی" />
