58869f97bb537463293371746498123257cb9fdc
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-08 10:21:32 +0330

    Image width is 90%

--- src/components/Body/FullyResponsive/FullyResponsive.jsx
+++ src/components/Body/FullyResponsive/FullyResponsive.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import fullyResponsive from "../../../assets/images/fullyResponsive.jpg"
+import { MouseParallaxContainer, MouseParallaxChild } from "react-parallax-mouse";
+import fullyResponsive from "../../../assets/images/fullyResponsive.jpg";
 
 function FullyResponsive() {
-
   return (
     <section className="bg-white flex-full-center flex-col pt-24 pb-14 px-4">
       <h3 className="mb-[4.6875rem] font-semibold text-3xl">[سازگار با انواع نمایشگرها]</h3>
-      <img className="w-full" src={fullyResponsive} alt="Fully Responsive" />
+      <MouseParallaxContainer globalFactorX={0.1} globalFactorY={0.1}>
+        <MouseParallaxChild className="flex-full-center" factorX={0.3} factorY={0.5}>
+          <img className="w-[90%]" src={fullyResponsive} alt="Fully Responsive" />
+        </MouseParallaxChild>
+      </MouseParallaxContainer>
     </section>
   );
 }
