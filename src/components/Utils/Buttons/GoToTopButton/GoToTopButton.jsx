ecc1e0c2d85d40299fc39e471f504a4e57b3ab88
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-08 09:01:58 +0330

    Handling button without using state or manipulate the dom

--- src/components/Utils/Buttons/GoToTopButton/GoToTopButton.jsx
+++ src/components/Utils/Buttons/GoToTopButton/GoToTopButton.jsx
@@ -1,28 +1,17 @@
 import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 function GoToTopButton() {
-  const [scrollHeight, setScrollHeight] = useState(false);
-  const root = document.getElementById("root");
   function handleScroll() {
-    root.scroll({
+    scroll({
       top: 0,
     });
   }
-  useEffect(() => {
-    root.addEventListener("scroll", () => {
-      if (root.scrollTop > 1000) {
-        setScrollHeight(true);
-      } else {
-        setScrollHeight(false);
-      }
-    });
-  }, [scrollHeight]);
-
+ 
   return (
     <>
-      <button onClick={handleScroll} className={`cursor-pointer bg-primary z-30 rounded-[4px] p-2 flex justify-center items-center border border-white fixed bottom-[5%] left-[5%] transition-all duration-300 w-10 h-10 hover:border-primary hover:bg-white hover:text-primary text-white ${scrollHeight ? "goToTopButton" : ""}`}>
+      <button onClick={handleScroll} className={`cursor-pointer bg-primary rounded-[4px] p-2 flex justify-center items-center border border-white fixed bottom-[5%] left-[5%] transition-all duration-300 w-10 h-10 hover:border-primary hover:bg-white hover:text-primary text-white ${scrollHeight ? "goToTopButton" : ""}`}>
         <FontAwesomeIcon icon={faAngleUp} />
       </button>
     </>
