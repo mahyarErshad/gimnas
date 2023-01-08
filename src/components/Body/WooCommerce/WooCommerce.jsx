04a94e8e19392351e85f229a5ab133e24de43986
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-08 10:23:09 +0330

    Given parallex effect

--- src/components/Body/WooCommerce/WooCommerce.jsx
+++ src/components/Body/WooCommerce/WooCommerce.jsx
@@ -1,10 +1,15 @@
 import React, { memo } from "react";
 import wooCommerce from "../../../assets/images/wooCommerce.png";
+import { MouseParallaxContainer, MouseParallaxChild } from "react-parallax-mouse";
 
 function WooCommerce() {
   return (
     <section className="bg-white flex-full-center pt-24 pb-14 px-4">
-      <img className="lg:w-[65.75rem] lg:h-[37.625rem] max-lg:w-full" src={wooCommerce} alt="WooCommerce" />
+      <MouseParallaxContainer globalFactorX={0.1} globalFactorY={0.1}>
+        <MouseParallaxChild className="flex-full-center" factorX={0.3} factorY={0.5}>
+          <img className="lg:w-[65.75rem] lg:h-[37.625rem] max-lg:w-full" src={wooCommerce} alt="WooCommerce" />
+        </MouseParallaxChild>
+      </MouseParallaxContainer>
     </section>
   );
 }
