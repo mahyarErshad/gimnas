c83acd15a5ab861bcc4d3c637294e0a8ab1b0d84
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-08 08:30:39 +0330

    Give bg color

--- src/components/Utils/Buttons/GoToTopButton/GoToTopButton.jsx
+++ src/components/Utils/Buttons/GoToTopButton/GoToTopButton.jsx
@@ -20,11 +20,9 @@ function GoToTopButton() {
 
   return (
     <>
-      {scrollHeight && (
-        <button onClick={handleScroll} className={`cursor-pointer rounded-[50%] p-2 flex justify-center items-center border border-white fixed bottom-[5%] left-[5%] bg-purple transition-all hover:bg-[#1f3fca] text-white ${scrollHeight ? "goToTopButton" : ""}`}>
-          <span className="material-symbols-outlined text-inherit">arrow_upward</span>
-        </button>
-      )}
+      <button onClick={handleScroll} className={`cursor-pointer bg-primary rounded-[50%] p-2 flex justify-center items-center border border-white fixed bottom-[5%] left-[5%] bg-purple transition-all hover:bg-[#1f3fca] text-white ${scrollHeight ? "goToTopButton" : ""}`}>
+        <span className="material-symbols-outlined text-inherit">arrow_upward</span>
+      </button>
     </>
   );
 }
