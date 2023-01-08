b0798ca6d091374a2d36344a026aa6c090552c6d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-08 09:28:56 +0330

    Counting is working using countUp package

--- src/components/Body/BodyParts/PageCounter/PageCounter.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounter.jsx
@@ -9,10 +9,10 @@ function PageCounter() {
     <section id="page-counter" className="bg-white relative z-10 py-[5.625rem] flex justify-center items-center flex-wrap max-md:px-4 md:px-8 lg:px-[15%] max-lg:gap-20">
       <HeaderSliderButton onClick={() => {modalContext.setIsModalOpen(true)}} title="پشتیبانی" image="https://img.icons8.com/windows/32/FFFFFF/help.png" top="top-[75%]" />
       <HeaderSliderButton onClick={() => window.open("https://www.rtl-theme.com/gimnas-sport-worpress-theme/", "_blank")} title="خرید قالب" image="https://img.icons8.com/external-creatype-outline-colourcreatype/28/FFFFFF/external-basket-miscellaneous-user-interface-v2-creatype-outline-colourcreatype.png" top="top-[81.5%]" />
-      <PageCounterComponent number="۱۲" text="تعداد صفحات اصلی" />
-      <PageCounterComponent number="۰۶" text="صفحات اصلی چندگانه" />
-      <PageCounterComponent number="۰۶" text="صفحات اصلی تکی" />
-      <PageCounterComponent plus number="۴۶" text="صفحات داخلی" />
+      <PageCounterComponent number={12} text="تعداد صفحات اصلی" />
+      <PageCounterComponent number={6} text="صفحات اصلی چندگانه" />
+      <PageCounterComponent number={6} text="صفحات اصلی تکی" />
+      <PageCounterComponent plus number={46} text="صفحات داخلی" />
     </section>
   );
 }
--- src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
+++ src/components/Body/BodyParts/PageCounter/PageCounterComponent.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import CountUp from "react-countup";
 
 function PageCounterComponent(props) {
   return (
     <div className="max-md:w-full md:w-[40%] lg:w-1/4 flex-col flex-full-center gap-8">
       <h3 className="text-[5rem] font-bold text-primary m-0 leading-4 flex js-count-up">
-        {props.number} {props.plus && <span className="text-inherit font-bold">+</span>}
+        <CountUp start={0} end={props.number} duration={2} delay={2} />
+        {props.plus && <span className="text-inherit font-bold">+</span>}
       </h3>
       <h4 className="text-xl font-bold m-0">{props.text}</h4>
     </div>
