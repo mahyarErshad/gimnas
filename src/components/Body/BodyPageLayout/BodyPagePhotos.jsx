466559ecf3d7c5befe5e2ec67412eea3875cd442
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-09 08:33:50 +0330

    Add data-aos

--- src/components/Body/BodyPageLayout/BodyPagePhotos.jsx
+++ src/components/Body/BodyPageLayout/BodyPagePhotos.jsx
@@ -2,7 +2,7 @@ import React, { memo } from "react";
 
 function BodyPagePhotos(props) {
   return (
-    <a target="_blank" href={props.href} className="pb-4 rounded-md transition-all hover:text-primary">
+    <a target="_blank" href={props.href} className="pb-4 rounded-md transition-all hover:text-primary" data-aos="fade-up">
       <img src={props.image} alt={props.title} className="custom-shadow max-md:w-[35rem] md:w-[21.5625rem] lg:w-[26.625rem]" />
       <h5 className="text-center mt-4 font-medium text-xl">{props.title}</h5>
     </a>
