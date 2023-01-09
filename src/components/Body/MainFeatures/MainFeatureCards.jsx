7013818dc130ae7fae781ede2c8f3f96e3aa8c35
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-09 08:51:48 +0330

    Add an animation using aos

--- src/components/Body/MainFeatures/MainFeatureCards.jsx
+++ src/components/Body/MainFeatures/MainFeatureCards.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function MainFeatureCards(props) {
   return (
-    <div className="shadow-md h-[10.75rem] main-feature-card bg-white transition-all duration-500 rounded-[5px] p-6 flex-full-center flex-col text-center max-md:w-[45%] md:w-[23%] hover:bg-primary hover:text-white">
+    <div className="shadow-md h-[10.75rem] main-feature-card bg-white transition-all duration-500 rounded-[5px] p-6 flex-full-center flex-col text-center max-md:w-[45%] md:w-[23%] hover:bg-primary hover:text-white" data-aos="flip-right" data-aos-duration="1000">
       <div className="w-[5rem] h-[5rem] rounded-[50%] bg-white flex-full-center mb-4">{props.children}</div>
       <h5 className="font-normal text-lg">{props.title}</h5>
     </div>
